0ffa369635592cdcbdabf5e08ed2f751a0541eaf
Zheng Yu Tay <zhengyu.tay@binance.com>
2020-04-15 13:45:00 +0800

    test(toolpack): use strictEqual

--- packages/toolpack/src/webpack/plugins/__tests__/fixtures/buildManifest/basic/webpack.config.js
+++ packages/toolpack/src/webpack/plugins/__tests__/fixtures/buildManifest/basic/webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
+    'page-1234': path.resolve(__dirname, 'page-1234.js'),
     index: path.resolve(__dirname, 'index.js'),
     another: path.resolve(__dirname, 'entry2.js'),
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
