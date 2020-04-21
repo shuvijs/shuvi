7f3aebade9a29e81a5c6074f4d3e920c06131d82
Tay Zheng Yu <32449414+ZhengYuTay@users.noreply.github.com>
2020-04-21 18:27:20 +0800

    test(toolpack): babel-preset unit test

---
+++ packages/toolpack/src/babel/__tests__/preset.test.ts
@@ -0,0 +1,165 @@
+import { transform } from '@babel/core';
+import type { CustomPresetOptions } from '../preset';
+import { trim } from 'test-utils';
+
+const babel = (
+  code: string,
+  { esm, isNode }: { esm: boolean; isNode: boolean } = {
+    esm: false,
+    isNode: true,
+  },
+  presetOptions: CustomPresetOptions = {}
+) =>
+  transform(code, {
+    filename: 'noop.js',
+    presets: [[require('../preset'), presetOptions]],
+    babelrc: false,
+    configFile: false,
+    sourceType: 'module',
+    compact: true,
+    caller: {
+      name: 'tests',
+      supportsStaticESM: esm,
+      isNode,
+    } as any,
+  })!.code;
+
+const BASIC_APP = `
+  import { useState } from 'react';
+  import styles from 'a.css';
+  import 'global.css';
+
+  const App = () => {
+      const [count, setCount] = useState(0);
+      return <div>{count}</div>
+  }
+
+  export default App;
+`;
+
+describe('shuvi/babel', () => {
+  const NODE_ENV = process.env.NODE_ENV;
+  beforeAll(() => {
+    process.env.NODE_ENV = 'production';
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+    process.env.NODE_ENV = NODE_ENV;
+  });
+
+  describe('node', () => {
+    const isNode = true;
+
+    test('basic app', () => {
+      const output = babel(trim(BASIC_APP), { esm: true, isNode });
+
+      // Do not convert arrow function
+      expect(output).toContain('const App=()=>{');
+
+      // Destructure array as object
+      expect(output).toContain('const{0:count,1:setCount}=useState(0);');
+
+      // Hoist React.createElement as __jsx
+      expect(output).toContain('var __jsx=React.createElement');
+      expect(output).toMatch(/return __jsx\(.*?\)/);
+
+      // Global css and css modules
+      expect(output).toMatch(/import styles from"a.css\?cssmodules"/);
+      expect(output).toMatch(/import'global.css'/);
+
+      expect(output).toMatchInlineSnapshot(
+        `"import React from\\"react\\";var __jsx=React.createElement;import{useState}from'react';import styles from\\"a.css?cssmodules\\";import'global.css';const App=()=>{const{0:count,1:setCount}=useState(0);return __jsx(\\"div\\",null,count);};export default App;"`
+      );
+    });
+
+    test('basic app without esm', () => {
+      const output = babel(trim(BASIC_APP), { esm: false, isNode });
+
+      // use strict
+      expect(output).toMatch(/^"use strict"/);
+
+      // require _interopRequireDefault
+      expect(output).toContain('function _interopRequireDefault');
+
+      // Global css and css modules
+      expect(output).toContain('require("global.css");');
+      expect(output).toContain('require("a.css?cssmodules")');
+
+      // Do not convert arrow function
+      expect(output).toContain('const App=()=>{');
+
+      // Destructure array as object
+      expect(output).toContain(
+        'const{0:count,1:setCount}=(0,_react.useState)(0);'
+      );
+
+      // Hoist React.createElement as __jsx
+      expect(output).toContain('var __jsx=_react.default.createElement;');
+      expect(output).toMatch(/return __jsx\(.*?\)/);
+
+      // Global css and css modules
+      expect(output).toContain('require("global.css");');
+      expect(output).toContain('require("a.css?cssmodules")');
+
+      // Exports
+      expect(output).toMatch(/var _default=App;exports.default=_default;$/);
+    });
+  });
+
+  describe('browser', () => {
+    const isNode = false;
+
+    test('basic app', () => {
+      const output = babel(trim(BASIC_APP), { esm: true, isNode });
+
+      // Global css and css modules
+      expect(output).toMatch(/import styles from"a.css\?cssmodules"/);
+      expect(output).toMatch(/import'global.css'/);
+
+      // Hoist React.createElement as __jsx
+      expect(output).toContain('var __jsx=React.createElement');
+      expect(output).toMatch(/return __jsx\(.*?\)/);
+
+      // Convert arrow function to function block
+      expect(output).toContain('var App=function App(){');
+
+      // Array to Object destructure and map array to individual variable
+      expect(output).toContain(
+        'var _useState=useState(0),count=_useState[0],setCount=_useState[1]'
+      );
+
+      expect(output).toMatchInlineSnapshot(
+        `"import React from\\"react\\";var __jsx=React.createElement;import{useState}from'react';import styles from\\"a.css?cssmodules\\";import'global.css';var App=function App(){var _useState=useState(0),count=_useState[0],setCount=_useState[1];return __jsx(\\"div\\",null,count);};export default App;"`
+      );
+    });
+
+    test('basic app without esm', () => {
+      const output = babel(trim(BASIC_APP), { esm: false, isNode });
+
+      // use strict
+      expect(output).toMatch(/^"use strict"/);
+
+      // require _interopRequireDefault
+      expect(output).toContain('var _interopRequireDefault=require');
+
+      // Global css and css modules
+      expect(output).toContain('require("global.css");');
+      expect(output).toContain('require("a.css?cssmodules")');
+
+      // Hoist React.createElement as __jsx
+      expect(output).toContain('var __jsx=_react["default"].createElement');
+
+      // Convert arrow function to function block
+      expect(output).toContain('var App=function App(){');
+
+      // Array to Object destructure and map array to individual variable
+      expect(output).toContain(
+        'var _useState=(0,_react.useState)(0),count=_useState[0],setCount=_useState[1];'
+      );
+
+      // export default
+      expect(output).toMatch(/exports\["default"\]=_default;$/);
+    });
+  });
+});
