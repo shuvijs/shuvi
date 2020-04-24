faabbb8b44198d3a48ecf3c70a7aed3cd643dea2
liximomo <liximomo@gmail.com>
2020-04-24 17:36:15 +0800

    feat: make shuvi to receive a partial config

--- packages/shuvi/src/shuvi/index.ts
+++ packages/shuvi/src/shuvi/index.ts
@@ -1,20 +1,22 @@
-import { IConfig } from "@shuvi/types";
-import Shuvi, { IShuviConstructorOptions } from "./shuvi.base";
+import { IConfig } from '@shuvi/types';
+import { deepmerge } from '@shuvi/utils/lib/deepmerge';
+import { defaultConfig } from '../config';
+import Shuvi, { IShuviConstructorOptions } from './shuvi.base';
 
 export { Shuvi };
 
 export interface ShuviOptions {
   dev?: boolean;
-  config: IConfig;
+  config: Partial<IConfig>;
 }
 
 export function shuvi({ dev = false, config }: ShuviOptions): Shuvi {
   let ShuviCtor: { new (options: IShuviConstructorOptions): Shuvi };
   if (dev) {
-    ShuviCtor = require("./shuvi.dev").default;
+    ShuviCtor = require('./shuvi.dev').default;
   } else {
-    ShuviCtor = require("./shuvi.prod").default;
+    ShuviCtor = require('./shuvi.prod').default;
   }
 
-  return new ShuviCtor({ config });
+  return new ShuviCtor({ config: deepmerge(defaultConfig, config) });
 }
