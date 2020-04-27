28d842b8e5bf11fc52a6da8398135e3b9f8c3ffe
liximomo <liximomo@gmail.com>
2020-04-27 17:28:52 +0800

    fix: using mjs in nodejs env

--- packages/toolpack/src/webpack/config/node.ts
+++ packages/toolpack/src/webpack/config/node.ts
@@ -1,9 +1,9 @@
-import WebpackChain from "webpack-chain";
-import { getTypeScriptInfo } from "@shuvi/utils/lib/detectTypescript";
-import { baseWebpackChain, BaseOptions } from "./base";
-import { nodeExternals } from "./parts/external";
-import { withStyle } from "./parts/style";
-import { resolvePreferTarget } from "./parts/resolve";
+import WebpackChain from 'webpack-chain';
+import { getTypeScriptInfo } from '@shuvi/utils/lib/detectTypescript';
+import { baseWebpackChain, BaseOptions } from './base';
+import { nodeExternals } from './parts/external';
+import { withStyle } from './parts/style';
+import { resolvePreferTarget } from './parts/resolve';
 
 export interface NodeOptions extends BaseOptions {}
 
@@ -13,35 +13,39 @@ export function createNodeWebpackChain({
   const chain = baseWebpackChain(baseOptions);
   const { useTypeScript } = getTypeScriptInfo(baseOptions.projectRoot);
 
-  chain.target("node");
+  chain.target('node');
   chain.devtool(false);
   const extensions = [
-    ...(useTypeScript ? [".tsx", ".ts"] : []),
-    ".js",
-    ".mjs",
-    ".jsx",
-    ".json",
-    ".wasm"
+    ...(useTypeScript ? ['.tsx', '.ts'] : []),
+    '.js',
+    '.mjs',
+    '.jsx',
+    '.json',
+    '.wasm'
   ];
   chain.resolve.extensions.merge(
     baseOptions.target
       ? resolvePreferTarget(baseOptions.target, extensions)
       : extensions
   );
-  chain.output.libraryTarget("commonjs2");
+  // fix: Can't reexport the named export 'BREAK' from non EcmaScript module
+  // related issue: https://github.com/graphql/graphql-js/issues/1272
+  chain.resolve.mainFields.clear().add('main').add('module');
+
+  chain.output.libraryTarget('commonjs2');
   chain.optimization.minimize(false);
   chain.externals(nodeExternals({ projectRoot: baseOptions.projectRoot }));
 
   chain.module
-    .rule("main")
-    .oneOf("js")
-    .use("babel-loader")
+    .rule('main')
+    .oneOf('js')
+    .use('babel-loader')
     .tap(options => ({
       ...options,
       isNode: true
     }));
 
-  chain.plugin("private/build-manifest").tap(([options]) => [
+  chain.plugin('private/build-manifest').tap(([options]) => [
     {
       ...options,
       modules: false
