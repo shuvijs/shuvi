6dc532f91d07ee2fc08c947d687e6b5a37452eea
liximomo <liximomo@gmail.com>
2020-05-08 17:12:54 +0800

    fix: ts type checking error report

--- test/utils/fixture.ts
+++ test/utils/fixture.ts
@@ -4,11 +4,15 @@ import { deepmerge } from '@shuvi/utils/lib/deepmerge';
 import { loadConfig } from 'shuvi/lib/config';
 import { build } from './build';
 
+export function resolveFixture(...paths: string[]) {
+  return path.resolve(__dirname, '..', 'fixtures', ...paths);
+}
+
 export async function loadFixture(
   fixture: string,
   overrides: Partial<IApiConfig> = {}
 ): Promise<IApiConfig> {
-  const rootDir = path.resolve(__dirname, '..', 'fixtures', fixture);
+  const rootDir = resolveFixture(fixture);
   const config = await loadConfig(rootDir);
 
   return deepmerge(config, overrides);
