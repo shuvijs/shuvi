35fd4e6b484c224e8621d86d76258eced391d747
liximomo <liximomo@gmail.com>
2020-05-14 16:36:06 +0800

    fix: fix broken when there are two shuvi

--- packages/core/src/app/models/files/FileNode.ts
+++ packages/core/src/app/models/files/FileNode.ts
@@ -1,8 +1,13 @@
-import { observable } from "mobx";
+import { observable } from 'mobx';
+
+const FILE_TYPE = Symbol.for('file');
+const DIR_TYPE = Symbol.for('dir');
 
 export type IFileNode = File | Dir;
 
 export class File {
+  $$type = FILE_TYPE;
+
   @observable name: string;
   @observable.ref type: React.ComponentType<any>;
   @observable props: any;
@@ -15,6 +20,8 @@ export class File {
 }
 
 export class Dir {
+  $$type = DIR_TYPE;
+
   @observable name: string;
 
   children: IFileNode[];
@@ -26,9 +33,9 @@ export class Dir {
 }
 
 export function isDir(obj: any): obj is Dir {
-  return obj instanceof Dir;
+  return obj && obj['$$type'] === DIR_TYPE;
 }
 
 export function isFile(obj: any): obj is File {
-  return obj instanceof File;
+  return obj && obj['$$type'] === FILE_TYPE;
 }
