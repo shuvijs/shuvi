c7e2dffd93abb4abd2c53f99481b189cc0d981d2
liximomo <liximomo@gmail.com>
2020-06-16 17:37:55 +0800

    test: application test

---
+++ packages/runtime-core/src/lib/__tests__/application.test.ts
@@ -0,0 +1,49 @@
+import { Application } from '../application';
+
+function getApp({ render }: any = {}) {
+  const app = new Application({
+    context: {},
+    AppComponent: {},
+    routes: [
+      {
+        id: 'test',
+        component: ''
+      }
+    ],
+    async render(options) {
+      return render && render(options);
+    }
+  });
+  return app;
+}
+
+describe('application', () => {
+  test('should add createAppContext hook', async () => {
+    const app = getApp();
+    app.tap('create-app-context', {
+      name: 'test',
+      fn(context: any) {
+        context.foo = 'bar';
+        return context;
+      }
+    });
+    await app.run();
+    const ctx = app.getContext();
+    expect(ctx.foo).toBe('bar');
+  });
+
+  test('should emit render-done event', async () => {
+    const render = jest.fn().mockReturnValue('render result');
+    const app = getApp({ render });
+    let renderResult;
+    app.on('render-done', (result: any) => {
+      renderResult = result;
+    });
+    await app.run();
+    expect(renderResult).toBe('render result');
+    const renderPrarmas = render.mock.calls[0][0];
+    expect(renderPrarmas['AppComponent']).toBeDefined();
+    expect(renderPrarmas['routes']).toBeDefined();
+    expect(renderPrarmas['appContext']).toBeDefined();
+  });
+});
