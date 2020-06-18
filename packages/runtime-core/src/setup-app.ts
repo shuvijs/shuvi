fcb1e6b16637d73627263aa441d34f673d261f41
liximomo <liximomo@gmail.com>
2020-06-18 11:33:12 +0800

    fix: router is undefined in app.js

--- packages/runtime-core/src/setup-app.ts
+++ packages/runtime-core/src/setup-app.ts
@@ -1,6 +1,8 @@
-import { create } from '@shuvi/app/core/application';
-import { renderer } from '@shuvi/app/core/renderer';
 import { CLIENT_CONTAINER_ID } from '@shuvi/shared/lib/constants';
+// renderer must be imported before application
+// we need to init init renderer before import AppComponent
+import { renderer } from '@shuvi/app/core/renderer';
+import { create } from '@shuvi/app/core/application';
 import { getAppData } from './lib/getAppData';
 
 const app = create(
