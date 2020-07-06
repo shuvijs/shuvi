3402d4f3008e9b6cb480b032f84864ce8365cc5e
Tay Zheng Yu <32449414+ZhengYuTay@users.noreply.github.com>
2020-07-06 19:47:20 +0800

    fix: spa default head tags

--- packages/shuvi/src/renderer/spa.ts
+++ packages/shuvi/src/renderer/spa.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_HEADS_TAGS } from '@shuvi/runtime-react/lib/head/constants';
 import { BaseRenderer } from './base';
 
 export class SpaRenderer extends BaseRenderer {
@@ -5,7 +6,7 @@ export class SpaRenderer extends BaseRenderer {
     const assets = this._getMainAssetTags();
     return {
       htmlAttrs: {},
-      headTags: [...assets.styles],
+      headTags: [...DEFAULT_HEADS_TAGS, ...assets.styles],
       mainTags: [this._getAppContainerTag()],
       scriptTags: [...assets.scripts]
     };
