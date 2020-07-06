f2ff2f4e1a096fcf8505b7ff3a0bcb6d49481a86
liximomo <liximomo@gmail.com>
2020-07-06 12:36:53 +0800

    test: add more cases for head

---
+++ test/fixtures/basic/src/pages/overwrite-default-head.js
@@ -0,0 +1,14 @@
+import { Head } from '@shuvi/app';
+
+export default () => (
+  <>
+    <Head>
+      <meta
+        data-foo="bar"
+        name="viewport"
+        content="width=device-width"
+      />
+    </Head>
+    <div id="head">Head Page</div>
+  </>
+);
