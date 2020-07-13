8624e2b5d50b14b94aab15cd241c657e373061a6
bowen zhu <609842385@qq.com>
2020-07-14 00:21:16 +0800

    feat: 🎸 add inspect command

---
+++ test/e2e/inspect.test.ts
@@ -0,0 +1,43 @@
+import { AppCtx, Page, launchFixture } from '../utils';
+
+jest.setTimeout(5 * 60 * 1000);
+
+afterEach(() => {
+  // force require to load file to make sure compiled file get load correctlly
+  jest.resetModules();
+});
+
+describe('Inspect Features', () => {
+  let ctx: AppCtx;
+  let page: Page;
+
+  beforeAll(async () => {
+    ctx = await launchFixture('inspect', { ssr: true });
+  });
+  afterAll(async () => {
+    await page.close();
+    await ctx.close();
+  });
+
+  test('should get DefinePlugin env in server correctly', async () => {
+    page = await ctx.browser.page(ctx.url('/'), {
+      disableJavaScript: true
+    });
+
+    const texts = await page.$$text('div p');
+
+    expect(texts[0]).toBe('Index Page');
+    expect(texts[1]).toBe('shuvi/server');
+    expect(texts[2]).toBe('false');
+  });
+
+  test('should get DefinePlugin env in client correctly', async () => {
+    page = await ctx.browser.page(ctx.url('/'));
+
+    const texts = await page.$$text('div p');
+
+    expect(texts[0]).toBe('Index Page');
+    expect(texts[1]).toBe('shuvi/client');
+    expect(texts[2]).toBe('false');
+  });
+});
