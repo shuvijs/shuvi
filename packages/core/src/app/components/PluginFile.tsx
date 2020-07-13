3baa320b3104969c32801d4bee76ade427009243
zhengyutay <zy>
2020-07-13 13:17:31 +0800

    - move plugin into a single file

--- packages/core/src/app/components/PluginFile.tsx
+++ packages/core/src/app/components/PluginFile.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { File } from '@shuvi/react-fs';
 import ModuleProxy from './files/ModuleProxy';
 import { useSelector } from '../models/store';
 
 function Plugin() {
   const source = useSelector(state => state.pluginModule);
+  const plugins = useSelector(state => state.runtimePlugins);
 
-  return <ModuleProxy name="plugin.js" source={source} defaultExport />;
+  let pluginsHash = '';
+  let content = '';
+
+  plugins.forEach((value, name) => {
+    content += `import ${name} from "${value}"\n`;
+    pluginsHash += `${name},`;
+  });
+
+  content += `let plugins = {${pluginsHash}}\n`;
+  content += `export default plugins;`;
+
+  return (
+    <>
+      <File name="pluginsHash.js" content={content} />
+      <ModuleProxy name="plugin.js" source={source} defaultExport />
+    </>
+  );
 }
 
 export default observer(Plugin);
