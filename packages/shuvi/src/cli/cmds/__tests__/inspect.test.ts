68ebd3ab201f1e35597d9627c0b2fef45815beeb
bowen-zhu <bowen.zhu@binance.com>
2020-07-15 17:19:39 +0800

    fix: 🐛 disable highlight in test environment

--- packages/shuvi/src/cli/cmds/__tests__/inspect.test.ts
+++ packages/shuvi/src/cli/cmds/__tests__/inspect.test.ts
@@ -1,11 +1,15 @@
-import { runShuviCommand } from 'shuvi-test-utils';
+import { createCliTestProject } from 'shuvi-test-utils';
 
 describe('shuvi inspect command', () => {
   test('should be log webpack config correctly', async () => {
     try {
-      const { message } = await runShuviCommand('inspect', [
-        'test/fixtures/inspect'
-      ]);
+      const project = createCliTestProject('test/fixtures/inspect');
+      const { message } = await project.run('inspect', [], {
+        env: {
+          ...process.env,
+          __DISABLE_HIGHLIGHT__: 'true'
+        }
+      });
       expect(message).toMatch(`mode: 'development'`);
       expect(message).toMatch(`name: 'shuvi/server'`);
       expect(message).toMatch(`name: 'shuvi/client'`);
@@ -18,30 +22,42 @@ describe('shuvi inspect command', () => {
   });
 
   test('can specify --mode', async () => {
-    const { message } = await runShuviCommand('inspect', [
-      'test/fixtures/inspect',
-      '--mode=production'
-    ]);
+    const project = createCliTestProject('test/fixtures/inspect');
+    const { message } = await project.run('inspect', ['--mode=production'], {
+      env: {
+        ...process.env,
+        __DISABLE_HIGHLIGHT__: 'true'
+      }
+    });
     expect(message).toMatch(`mode: 'production'`);
     expect(message).toMatch(`name: 'shuvi/server'`);
     expect(message).toMatch(`name: 'shuvi/client'`);
   });
 
   test('can specify --verbose', async () => {
-    const { message } = await runShuviCommand('inspect', [
-      'test/fixtures/inspect',
-      '--verbose'
-    ]);
+    const project = createCliTestProject('test/fixtures/inspect');
+    const { message } = await project.run('inspect', ['--verbose'], {
+      env: {
+        ...process.env,
+        __DISABLE_HIGHLIGHT__: 'true'
+      }
+    });
     expect(message).toMatch(`'process.env.NODE_ENV': '"development"'`);
     expect(message).toMatch(`__NAME__: '"shuvi/client"'`);
     expect(message).toMatch(`__NAME__: '"shuvi/server"'`);
     expect(message).toMatch(`__DEV__: true`);
 
-    const { message: message2 } = await runShuviCommand('inspect', [
-      'test/fixtures/inspect',
-      '--mode=production',
-      '--verbose'
-    ]);
+    const project2 = createCliTestProject('test/fixtures/inspect');
+    const { message: message2 } = await project2.run(
+      'inspect',
+      ['test/fixtures/inspect', '--mode=production', '--verbose'],
+      {
+        env: {
+          ...process.env,
+          __DISABLE_HIGHLIGHT__: 'true'
+        }
+      }
+    );
     expect(message2).toMatch(`'process.env.NODE_ENV': '"production"'`);
     expect(message2).toMatch(`__NAME__: '"shuvi/client"'`);
     expect(message2).toMatch(`__NAME__: '"shuvi/server"'`);
@@ -50,7 +66,8 @@ describe('shuvi inspect command', () => {
 
   test('should exit process when dir is not exist', async () => {
     try {
-      await runShuviCommand('inspect', ['test/fixtures/xxx']);
+      const project = createCliTestProject('test/fixtures/inspect');
+      await project.run('inspect');
     } catch (e) {
       expect(e.code).toBe(1);
       expect(e.message).toMatch('No such directory exists as the project root');
