f6b928d4f8e40e839f8a687de0d263c706f05d30
liximomo <liximomo@gmail.com>
2020-08-20 11:18:01 +0800

    feat: add router event

--- packages/runtime-react/src/shuvi-app/router/router.ts
+++ packages/runtime-react/src/shuvi-app/router/router.ts
@@ -1,3 +1,4 @@
+import createEvent, { EventEmitter } from '@shuvi/utils/lib/eventEmitter';
 import { Runtime } from '@shuvi/types';
 import { History } from './history';
 
@@ -6,10 +7,17 @@ export { RouteProps } from 'react-router-dom';
 export type Router = Runtime.IRouter;
 
 let history: History;
+let routerEvent: EventEmitter;
 
 //@internal
 export function setHistory(value: History) {
   history = value;
+  routerEvent = createEvent();
+}
+
+//@internal
+export function emitRouterEvent(event: string) {
+  routerEvent && routerEvent.emit(event);
 }
 
 function push(...args: any[]) {
@@ -41,6 +49,18 @@ function onChange(listener: Runtime.IRouterListener) {
   return history.listen(listener);
 }
 
+function onRouteChangeStart(listener: Runtime.IRouterListener) {
+  return history.listen(listener);
+}
+
+function onRouteChangeComplete(listener: () => void) {
+  routerEvent.on('route-change-complete', listener);
+
+  return () => {
+    routerEvent.off('route-change-complete', listener);
+  };
+}
+
 const router: Router = {
   get query() {
     return history.location.query;
@@ -53,7 +73,9 @@ const router: Router = {
   go,
   goBack,
   goForward,
-  onChange
+  onChange,
+  onRouteChangeStart,
+  onRouteChangeComplete
 };
 
 export default router;
