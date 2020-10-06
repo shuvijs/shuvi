fb2deff27a7f25cd0330fd0e91a1277e2348aedd
Michael Hsu <evenchange4@gmail.com>
2020-10-06 12:37:46 +0800

    fix(deepmerge): should not mutate the input object [#76] (#77)

--- packages/utils/src/deepmerge.ts
+++ packages/utils/src/deepmerge.ts
@@ -2,22 +2,23 @@ function mergeTwoObject(origin: any, target: any) {
   if (target === null || typeof target === 'undefined') {
     return origin;
   }
+  const result = { ...origin };
 
-  Object.keys(target).forEach((key) => {
-    const originValue = origin[key];
+  Object.keys(target).forEach(key => {
+    const originValue = result[key];
     const targetValue = target[key];
     if (Array.isArray(originValue) || Array.isArray(targetValue)) {
-      origin[key] = targetValue;
+      result[key] = targetValue;
     } else if (
       typeof originValue === 'object' &&
       typeof targetValue === 'object'
     ) {
-      origin[key] = mergeTwoObject(originValue, targetValue);
+      result[key] = mergeTwoObject(originValue, targetValue);
     } else {
-      origin[key] = targetValue;
+      result[key] = targetValue;
     }
   });
-  return origin;
+  return result;
 }
 
 export function deepmerge(...args: any[]) {
