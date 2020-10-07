120e2be3dbf752c466c061b76c948a4681196dcb
zhengyutay <zy>
2020-10-07 12:42:00 +0800

    chore: upgrade non-css related webpack plugin
    - fix fork-ts-checker breaking change
    - remove double error log from build

--- packages/toolpack/src/utils/forkTsCheckerWebpackPlugin.ts
+++ packages/toolpack/src/utils/forkTsCheckerWebpackPlugin.ts
@@ -1,4 +1,4 @@
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-export { createCodeframeFormatter } from "fork-ts-checker-webpack-plugin/lib/formatter/codeframeFormatter";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+export { createCodeFrameFormatter } from 'fork-ts-checker-webpack-plugin/lib/formatter/codeframeFormatter';
 
 export default ForkTsCheckerWebpackPlugin;
