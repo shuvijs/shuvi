207b15fa8dd1dcae2da26738a5b8e5204b572494
Michael Hsu <evenchange4@gmail.com>
2020-10-22 17:42:09 +0800

    fix: use koa ctx.response api

--- packages/shuvi/src/lib/sendHtml.ts
+++ packages/shuvi/src/lib/sendHtml.ts
@@ -3,9 +3,9 @@ import { Runtime } from '@shuvi/types';
 export function sendHTML(ctx: Runtime.IKoaContext, html: string) {
   if (ctx.res.writableEnded || ctx.res.headersSent) return;
 
-  if (!ctx.response.headers['Content-Type']) {
-    ctx.response.set('Content-Type', 'text/html; charset=utf-8');
+  if (!ctx.response.type) {
+    ctx.response.type = 'text/html; charset=utf-8';
   }
-  ctx.response.set('Content-Length', Buffer.byteLength(html).toString());
+  ctx.response.length = Buffer.byteLength(html);
   ctx.body = ctx.request.method === 'HEAD' ? null : html;
 }
