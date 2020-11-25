a7c80bf338baddc4abd09c864446d6bd7e05c9eb
X.L <liximomo@gmail.com>
2020-11-26 00:33:28 +0800

    fix(module-replace-plugin): compatibility with webpack@5 cache (#114)

--- packages/toolpack/src/webpack/plugins/module-replace-plugin/stub-loader.ts
+++ packages/toolpack/src/webpack/plugins/module-replace-plugin/stub-loader.ts
@@ -1,12 +1,45 @@
 import { loader } from 'webpack';
 import loaderUtils from 'loader-utils';
 
-module.exports = function (this: loader.LoaderContext) {
-  const { module }: any = loaderUtils.getOptions(this) || {};
-  const sModule = JSON.stringify(module);
+const isSelfLoader = (l: any) => l.path !== __filename;
+
+const genRequest = (loaderCtx: loader.LoaderContext, loaders: any[]) => {
+  const loaderStrings: string[] = [];
+
+  loaders.forEach(loader => {
+    const request = typeof loader === 'string' ? loader : loader.request;
+    // loader.request contains both the resolved loader path and its options
+    // query (e.g. ??ref-0)
+    loaderStrings.push(request);
+  });
+
+  return loaderUtils.stringifyRequest(
+    loaderCtx,
+    '!' +
+      [...loaderStrings, loaderCtx.resourcePath + loaderCtx.resourceQuery].join(
+        '!'
+      )
+  );
+};
+
+module.exports = (code: string) => code;
+
+module.exports.pitch = function (this: loader.LoaderContext) {
   this.cacheable(false);
 
+  const { replacedModule }: any = loaderUtils.getOptions(this) || {};
+  let loaders = this.loaders;
+
+  // remove self
+  loaders = loaders.filter(isSelfLoader);
+
+  const request = replacedModule
+    ? loaderUtils.stringifyRequest(this, replacedModule)
+    : genRequest(this, loaders);
+
   return `
-module.exports = require(${sModule})
+import mod from ${request}; 
+export * from ${request}
+export default mod;
 `.trim();
 };
