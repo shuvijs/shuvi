52353a62028f8d95c3b888322c5a2ffc4a5d8d75
Tay Zheng Yu <32449414+ZhengYuTay@users.noreply.github.com>
2021-03-23 08:30:33 +0800

    fix: bug where 404 page is not returning 404 statusCode (#145)
    Co-authored-by: zhengyutay <zy>

--- packages/shuvi/src/lib/sendHtml.ts
+++ packages/shuvi/src/lib/sendHtml.ts
@@ -7,5 +7,9 @@ export function sendHTML(ctx: Runtime.IServerAppContext, html: string) {
     ctx.response.type = 'text/html; charset=utf-8';
   }
   ctx.response.length = Buffer.byteLength(html);
+
+  // ctx.body will set ctx.status to 200, if ctx.status is not set before
+  ctx.status = ctx.res.statusCode;
+
   ctx.body = ctx.request.method === 'HEAD' ? null : html;
 }
