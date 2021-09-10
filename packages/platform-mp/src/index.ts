7974574e008001de678ba99e33122c942ae78176
Tom <214997474@qq.com>
2021-09-10 15:45:45 +0800

    feat(platform-mp): rename binance to bmp

--- packages/platform-mp/src/index.ts
+++ packages/platform-mp/src/index.ts
@@ -3,7 +3,7 @@ import { IApi, Runtime } from '@shuvi/types';
 
 const platform: Runtime.IRuntime<any> = {
   async install(api: IApi): Promise<void> {
-    const { target = 'binance' } = api.config.platform || {};
+    const { target = 'bmp' } = api.config.platform || {};
     const PlatformConstructor: Runtime.IRuntime = require(`./targets/${target}`)
       .default;
     //@ts-ignore
