ebf14ae1c3304d980552f683640554a7d9cbe6f8
liximomo <liximomo@gmail.com>
2021-09-18 12:05:54 +0800

    fix: types

--- test/fixtures/typescript/src/apis/index.ts
+++ test/fixtures/typescript/src/apis/index.ts
@@ -1,7 +1,8 @@
 type resData = {
   data: string;
 };
-export default (req, res) => {
+
+export default (req: any, res: any) => {
   const data: resData = { data: 'apis index success' };
   return res.send(data);
 };
