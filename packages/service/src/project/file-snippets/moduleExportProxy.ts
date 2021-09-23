cb632ba13be709b5c8ce6e86810372946c18ffb3
Repraance <reprance@gmail.com>
2021-09-23 10:09:18 +0800

    fix(service): fix removeExt (#192)

--- packages/service/src/project/file-snippets/moduleExportProxy.ts
+++ packages/service/src/project/file-snippets/moduleExportProxy.ts
@@ -1,6 +1,7 @@
 import { watch } from '@shuvi/utils/lib/fileWatcher';
 import { reactive } from '../file-manager';
 import fse from 'fs-extra';
+import path from 'path';
 
 export const findFirstExistedFile = (files: string[]): string | null => {
   for (let index = 0; index < files.length; index++) {
@@ -72,7 +73,8 @@ const getWatcher = (
 };
 
 export function removeExt(filePath: string): string {
-  return filePath.replace(/(.*)\.(.*?)$/, '$1');
+  const parsed = path.parse(filePath);
+  return path.join(parsed.dir, parsed.name);
 }
 
 export const moduleExportProxyCreater = () => {
