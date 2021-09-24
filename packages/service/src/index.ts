fee965d3a7314c6944d62e38574be5295f1ba747
Repraance <reprance@gmail.com>
2021-09-25 00:30:26 +0800

    feat(service): export all the constants (#196)

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -5,12 +5,4 @@ export { matchRoutes } from '@shuvi/router';
 export { shuvi, Shuvi } from './shuvi';
 export { IApi, Api, getApi, PluginApi, IApiConfig, IConfig } from './api';
 export { ProjectBuilder } from './project';
-export {
-  BUNDLER_TARGET_CLIENT,
-  BUNDLER_TARGET_SERVER,
-  IDENTITY_RUNTIME_PUBLICPATH,
-  PHASE_PRODUCTION_BUILD,
-  PHASE_PRODUCTION_SERVER,
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_INSPECT_WEBPACK
-} from './constants';
+export * from './constants';
