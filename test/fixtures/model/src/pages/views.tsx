d9b95bce8f09cb4352739cc6c06c77fc71a99992
jinyangwang <214997474@qq.com>
2022-01-04 10:16:56 +0800

    feat: remove reselector

--- test/fixtures/model/src/pages/views.tsx
+++ test/fixtures/model/src/pages/views.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 import { useModel, defineModel } from '@shuvi/services/store';
 
+// todo: test model state should be object
+const error = defineModel({
+  name: 'error',
+  state: {},
+  reducers: {}
+});
+
 const other = defineModel({
   name: 'other',
   state: {
