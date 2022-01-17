6ef73fdda8b5b10e1f7654c995f3a5865a892851
Tom <214997474@qq.com>
2022-01-17 22:37:29 +0800

    fix: remove keyv warning

--- packages/toolpack/src/webpack/config/base.ts
+++ packages/toolpack/src/webpack/config/base.ts
@@ -306,5 +306,10 @@ export function baseWebpackChain({
       .use(webpack.ids.HashedModuleIdsPlugin);
   }
 
+  // https://github.com/jaredwray/keyv/issues/45#issuecomment-755814594
+  config
+    .plugin('private/keyv')
+    .use(new webpack.ContextReplacementPlugin(/keyv/));
+
   return config;
 }
