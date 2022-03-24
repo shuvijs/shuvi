e89a45deb275cee0d90a339302ad1bac4576f603
Tom <214997474@qq.com>
2022-03-24 12:48:56 +0800

    chore(test): complex runtime.js test

--- test/fixtures/custom-runtime/src/pages/index.js
+++ test/fixtures/custom-runtime/src/pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>Index Page</div>;
+export default function Index(props) {
+  return <div>Index Page: {props.index}</div>;
+}
+Index.getInitialProps = function () {
+  return {
+    index: 'index props'
+  };
+};
