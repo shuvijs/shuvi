e82a461cf0a124a16f1801f1e345ded58b1997fa
Tom <214997474@qq.com>
2022-03-24 12:44:45 +0800

    chore(test): complex runtime.js test

--- test/fixtures/custom-runtime/src/runtime.js
+++ test/fixtures/custom-runtime/src/runtime.js
@@ -1,1 +1,27 @@
-export * from './customRuntime';
+export const getRootAppComponent = App => {
+  function RootAppComponent(props) {
+    return (
+      <div>
+        <div>This is getRootAppComponent</div>
+        <App {...props} />
+      </div>
+    );
+  }
+  if (App.getInitialProps)
+    RootAppComponent.getInitialProps = App.getInitialProps;
+  return RootAppComponent;
+};
+
+export const getAppComponent = UserApp => {
+  function AppComponent(props) {
+    return (
+      <div>
+        <div>This is getAppComponent</div>
+        <UserApp {...props} />
+      </div>
+    );
+  }
+  if (UserApp.getInitialProps)
+    AppComponent.getInitialProps = UserApp.getInitialProps;
+  return AppComponent;
+};
