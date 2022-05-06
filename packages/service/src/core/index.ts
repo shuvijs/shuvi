de81ca532f44233a26d88f245ee970bd20b3c11e
Repraance <reprance@gmail.com>
2022-05-06 14:27:42 +0800

    feat: swc-loader can process runtimePlugin

--- packages/service/src/core/index.ts
+++ packages/service/src/core/index.ts
@@ -1,5 +1,6 @@
 export type { Api } from './api';
 export { getApi } from './api';
 export { loadConfig, defineConfig, getFullUserConfig } from './config';
+export { resolvePlugin } from './getPlugins';
 export * from './apiTypes';
 export * from './lifecycle';
