88b30330ad67e4a3fe1dfc80ccd8c5a0552d3233
Yu-An Chang <j60508888@gmail.com>
2022-05-30 17:35:10 +0800

    refactor: runtime config default to private

--- packages/platform-shared/src/platform/plugin/hooks.ts
+++ packages/platform-shared/src/platform/plugin/hooks.ts
@@ -7,7 +7,10 @@ import { IRuntimeConfig } from '@shuvi/service/lib/core';
 const addEntryCode = createAsyncParallelHook<void, void, string | string[]>();
 const addPolyfill = createAsyncParallelHook<void, void, string | string[]>();
 const modifyRuntimeConfig = createAsyncSeriesWaterfallHook<
-  IRuntimeConfig,
+  {
+    public: IRuntimeConfig;
+    server: IRuntimeConfig;
+  },
   void
 >();
 
