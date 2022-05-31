fe458082718cb643171d1c6e73a9c1ca8242f06d
ives <mine@ives.pro>
2022-05-31 21:41:59 +0800

    feat(react 18.0):回滚测试用例。

--- test/fixtures/model/src/pages/index.tsx
+++ test/fixtures/model/src/pages/index.tsx
@@ -1,119 +1,117 @@
-// import { useModel, defineModel } from '@shuvi/runtime/model';
-//
-// const sleep = (time: number) =>
-//   new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve(null);
-//     }, time);
-//   });
-//
-// const core = defineModel({
-//   name: 'core',
-//   state: {
-//     hello: 'core',
-//     step: 1
-//   },
-//   reducers: {
-//     addStep: state => {
-//       return {
-//         ...state,
-//         step: state.step + 1
-//       };
-//     }
-//   },
-//   effects: {
-//     async addStepAsync() {
-//       await sleep(1000);
-//       this.addStep();
-//     }
-//   }
-// });
-//
-// const base = defineModel(
-//   {
-//     name: 'base',
-//     state: {
-//       hello: 'base',
-//       step: 1
-//     },
-//     reducers: {
-//       addStep: state => {
-//         return {
-//           ...state,
-//           step: state.step + 1
-//         };
-//       }
-//     },
-//     effects: {
-//       async addStepAsync() {
-//         await sleep(1000);
-//         this.addStep();
-//       }
-//     }
-//   },
-//   [core]
-// );
-//
-// const count = defineModel(
-//   {
-//     name: 'count',
-//     state: {
-//       hello: 'count',
-//       num: 1
-//     },
-//     reducers: {
-//       addCount: (state, payload: number) => {
-//         return {
-//           ...state,
-//           num: state.num + payload
-//         };
-//       }
-//     },
-//     effects: {
-//       async addCountAsync() {
-//         this.addCount(this.$dep.base.$state().step);
-//         await this.$dep.base.addStepAsync();
-//       }
-//     }
-//   },
-//   [base]
-// );
-//
-// export default function Index() {
-//   const [{ num, hello: helloCount }, { addCountAsync }] = useModel(count);
-//   const [{ hello: helloBase, step }, { addStepAsync }] = useModel(base);
-//   return (
-//     <div>
-//       <div>
-//         Count: {helloCount} {num}
-//         <button
-//           onClick={() => {
-//             addCountAsync();
-//           }}
-//         >
-//           Add Count
-//         </button>
-//       </div>
-//
-//       <div>
-//         Base: {helloBase} {step}
-//         <button
-//           onClick={() => {
-//             addStepAsync();
-//           }}
-//         >
-//           Add Step
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-//
-// Index.getInitialProps = async function (ctx: any) {
-//   const modelManager = ctx.appContext.modelManager;
-//   const baseStore = modelManager.get(base);
-//   await baseStore.addStepAsync();
-//   console.log(baseStore.$state());
-// };
+import { useModel, defineModel } from '@shuvi/runtime/model';
 
-export {};
+const sleep = (time: number) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve(null);
+    }, time);
+  });
+
+const core = defineModel({
+  name: 'core',
+  state: {
+    hello: 'core',
+    step: 1
+  },
+  reducers: {
+    addStep: state => {
+      return {
+        ...state,
+        step: state.step + 1
+      };
+    }
+  },
+  effects: {
+    async addStepAsync() {
+      await sleep(1000);
+      this.addStep();
+    }
+  }
+});
+
+const base = defineModel(
+  {
+    name: 'base',
+    state: {
+      hello: 'base',
+      step: 1
+    },
+    reducers: {
+      addStep: state => {
+        return {
+          ...state,
+          step: state.step + 1
+        };
+      }
+    },
+    effects: {
+      async addStepAsync() {
+        await sleep(1000);
+        this.addStep();
+      }
+    }
+  },
+  [core]
+);
+
+const count = defineModel(
+  {
+    name: 'count',
+    state: {
+      hello: 'count',
+      num: 1
+    },
+    reducers: {
+      addCount: (state, payload: number) => {
+        return {
+          ...state,
+          num: state.num + payload
+        };
+      }
+    },
+    effects: {
+      async addCountAsync() {
+        this.addCount(this.$dep.base.$state().step);
+        await this.$dep.base.addStepAsync();
+      }
+    }
+  },
+  [base]
+);
+
+export default function Index() {
+  const [{ num, hello: helloCount }, { addCountAsync }] = useModel(count);
+  const [{ hello: helloBase, step }, { addStepAsync }] = useModel(base);
+  return (
+    <div>
+      <div>
+        Count: {helloCount} {num}
+        <button
+          onClick={() => {
+            addCountAsync();
+          }}
+        >
+          Add Count
+        </button>
+      </div>
+
+      <div>
+        Base: {helloBase} {step}
+        <button
+          onClick={() => {
+            addStepAsync();
+          }}
+        >
+          Add Step
+        </button>
+      </div>
+    </div>
+  );
+}
+
+Index.getInitialProps = async function (ctx: any) {
+  const modelManager = ctx.appContext.modelManager;
+  const baseStore = modelManager.get(base);
+  await baseStore.addStepAsync();
+  console.log(baseStore.$state());
+};
