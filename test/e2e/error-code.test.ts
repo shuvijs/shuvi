12231af5e8d99e2f478957c7646c2c0d1f157448
ives <mine@ives.pro>
2022-06-09 12:11:54 +0800

    fix(remove appContext statusCode):增加e2e测试用例

---
+++ test/e2e/error-code.test.ts
@@ -0,0 +1,34 @@
+import type { AppCtx } from "shuvi-test-utils";
+import type { Page } from '../utils'
+import { launchFixture } from "shuvi-test-utils";
+
+jest.setTimeout(5 * 60 * 1000);
+
+describe('error code test', function () {
+
+  let ctx:AppCtx;
+  let page:Page
+
+  beforeAll(async () => {
+    ctx = await launchFixture("error-code")
+    page = await ctx.browser.page(ctx.url("/"));
+    await page.waitForSelector("#__APP")
+  })
+
+  it('http status code is 404', function () {
+    expect(page.statusCode).toBe(404);
+  });
+
+  test("render status correct",async () => {
+    const text = await page.$text("#__APP  > div > div > div:first-child")
+    expect(text).toBe("404");
+  })
+
+  test("render description correct",async () => {
+    const text = await page.$text("#__APP  > div > div > div:nth-child(2)")
+    expect(text).toBe("page not found");
+
+  })
+
+
+});
