457cf2018767f85f2ee17a92298c38747d80091a
ives <mine@ives.pro>
2022-06-10 11:52:30 +0800

    :sparkles: feat(layout rfc): 编写集成测试用例。

---
+++ packages/service/src/route-layout/index.ts
@@ -0,0 +1,1 @@
+export { getRoutesWithLayoutFromDir } from './route';
--- test/fixtures/error-code/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-function Index() {
-  return <div>index</div>
-}
-
-Index.getInitialProps = ctx => {
-  ctx.error(404,"page not found")
-}
-
-export default Index
