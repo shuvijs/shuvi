6993f616f6f8fc590e8387c8c77482bbf5257a33
myron <myron@gmail.com>
2022-06-23 21:51:15 +0800

    chore: upgrade to TypeScript@4.7

--- packages/service/src/bundler/typescript/configTypeScript.ts
+++ packages/service/src/bundler/typescript/configTypeScript.ts
@@ -49,7 +49,7 @@ export async function writeDefaultConfigurations(
       // All of these values work:
       parsedValues: [
         ts.ModuleResolutionKind.NodeJs,
-        ts.ModuleResolutionKind.Node12,
+        ts.ModuleResolutionKind.Node16,
         ts.ModuleResolutionKind.NodeNext
       ],
       value: 'node',
