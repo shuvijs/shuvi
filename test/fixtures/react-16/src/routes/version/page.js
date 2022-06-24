3d37fb50a8bf398289d7ce5289b01bb28f72e68a
ives <mine@ives.pro>
2022-06-24 11:54:58 +0800

    :sparkles: feat(layout rfc): 迁移public dir、public path、react 16 runtime config project with type error

--- test/fixtures/react-16/src/routes/version/page.js
+++ test/fixtures/react-16/src/routes/version/page.js
@@ -4,8 +4,8 @@ import { version as domVersion } from 'react-dom';
 export default () => {
   return (
     <div id="version">
-      <div id="reactVersion">{ version }</div>
-      <div id="domVersion">{ domVersion }</div>
+      <div id="reactVersion">{version}</div>
+      <div id="domVersion">{domVersion}</div>
     </div>
   );
-}
+};
