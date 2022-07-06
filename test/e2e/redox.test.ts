672c679acf4fa935afe304a388764c04130d5c3b
jinyangwang <214997474@qq.com>
2022-07-06 09:48:51 +0800

    test: add redox test (#338)

---
+++ test/e2e/redox.test.ts
@@ -0,0 +1,35 @@
+import { AppCtx, Page, launchFixture } from '../utils';
+
+jest.setTimeout(5 * 60 * 1000);
+
+describe('redox', () => {
+  beforeAll(async () => {
+    ctx = await launchFixture('redox');
+    page = await ctx.browser.page();
+  });
+  afterAll(async () => {
+    await page.close();
+    await ctx.close();
+  });
+  let ctx: AppCtx;
+  let page: Page;
+
+  test('ssr state delivery to client', async () => {
+    await page.goto(ctx.url('/'));
+    await page.waitForSelector('#step');
+    expect(await page.$text('#step')).toContain('2');
+  });
+
+  test('async actions should worked', async () => {
+    await page.goto(ctx.url('/'));
+    await page.waitForSelector('#step');
+    // @ts-ignore
+    await page.$eval('#add-async', el => el.click());
+    await new Promise(resolve => {
+      setTimeout(function () {
+        return resolve(null);
+      }, 150);
+    });
+    expect(await page.$text('#step')).toContain('3');
+  });
+});
