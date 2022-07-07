685021aa215538e2c62bc16285de7d8f5679f845
Li Qiang <Ives7@users.noreply.github.com>
2022-07-07 10:49:00 +0800

    chore: rename runtime.js to app.js

--- packages/platform-shared/src/node/platform/plugins/model/runtime.ts
+++ packages/platform-shared/src/node/platform/plugins/model/runtime.ts
@@ -37,7 +37,7 @@ const initStore: InitRedox = ({ initialState, ctx }) => {
 };
 
 export default createPlugin({
-  getAppContext: ctx => {
+  appContext: ctx => {
     if (!ctx.storeManager) {
       let initialState = {};
       if (!isServer) {
