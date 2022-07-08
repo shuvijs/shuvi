81b371bb1ee45ee3659336e84b32868cac02e672
myron <myron@gmail.com>
2022-07-08 11:20:34 +0800

    refactor: clear some hooks

--- test/packages/plugin-use-exports/src/server.ts
+++ test/packages/plugin-use-exports/src/server.ts
@@ -1,7 +1,7 @@
 import { createServerPlugin, ServerPluginInstance } from '@shuvi/service';
 
 export default createServerPlugin({
-  onListen: () => {
+  listen: () => {
     console.warn('plugin-use-exports server');
   }
 }) as ServerPluginInstance;
