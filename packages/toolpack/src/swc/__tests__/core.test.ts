e851dc7e6843d085f2fbeabbbf4597ed78fda66e
Tom <21499747@qq.com>
2022-07-15 10:27:48 +0800

    chore: remove shuvi-swc-loader.test.js

--- packages/toolpack/src/swc/__tests__/core.test.ts
+++ packages/toolpack/src/swc/__tests__/core.test.ts
@@ -3,10 +3,12 @@ import { trim } from 'shuvi-test-utils';
 
 const swc = async (
   code: string,
-  { esm, isNode }: { esm: boolean; isNode: boolean }
+  {
+    esm = true,
+    isNode = false,
+    filename = 'noop.js'
+  }: { esm?: boolean; isNode?: boolean; filename?: string } = {}
 ) => {
-  const filename = 'noop.js';
-
   const isTSFile = filename.endsWith('.ts');
   const isTypeScript = isTSFile || filename.endsWith('.tsx');
   const development = process.env.NODE_ENV === 'development';
@@ -28,6 +30,14 @@ const swc = async (
         development,
         useBuiltins: true,
         refresh: false
+      },
+      optimizer: {
+        simplify: false,
+        globals: {
+          typeofs: {
+            window: isNode ? 'undefined' : 'object'
+          }
+        }
       }
     }
   };
@@ -71,7 +81,7 @@ const BASIC_APP = `
   export default App;
 `;
 
-describe('shuvi/swc', () => {
+describe('swc/core', () => {
   const NODE_ENV = process.env.NODE_ENV;
   beforeEach(() => {
     // @ts-ignore
@@ -141,6 +151,22 @@ describe('shuvi/swc', () => {
       // Exports
       expect(output).toMatch(/var _default=App;exports.default=_default$/);
     });
+
+    describe('replace constants', () => {
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window !== 'undefined';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"undefined\\"!=='undefined'"`);
+      });
+
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window !== 'object';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"undefined\\"!=='object'"`);
+      });
+    });
   });
 
   describe('browser', () => {
@@ -196,6 +222,174 @@ describe('shuvi/swc', () => {
 
       // export default
       expect(output).toMatch(/exports\.default=_default$/);
+    });
+
+    describe('replace constants', () => {
+      test('should replace typeof window expression nested', async () => {
+        const code = await swc('function a(){console.log(typeof window)}', {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(
+          `"function a(){console.log(\\"object\\")}"`
+        );
+      });
+
+      test('should replace typeof window expression nested', async () => {
+        const code = await swc(
+          `function a(){console.log(typeof window === 'undefined')}`,
+          { isNode }
+        );
+        expect(code).toMatchInlineSnapshot(
+          `"function a(){console.log(\\"object\\"==='undefined')}"`
+        );
+      });
+
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window === 'undefined';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"object\\"==='undefined'"`);
+      });
+
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window === 'object';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"object\\"==='object'"`);
+      });
+
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window !== 'undefined';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"object\\"!=='undefined'"`);
+      });
+
+      test('should replace typeof window expression top level', async () => {
+        const code = await swc(`typeof window !== 'object';`, {
+          isNode
+        });
+        expect(code).toMatchInlineSnapshot(`"\\"object\\"!=='object'"`);
+      });
+    });
+  });
+
+  describe('syntax', () => {
+    test('should support exportDefaultFrom', async () => {
+      const code = await swc(`export { default as foo } from "bar"`);
+      expect(code).toMatchInlineSnapshot(
+        `"export{default as foo}from\\"bar\\""`
+      );
+    });
+
+    test('should not drop unused exports by default', async () => {
+      const code = await swc(
+        // effectful
+        `import"core-js";` +
+          // basic
+          `import{foo,bar}from"a";import baz from"b";` +
+          // complex
+          `import * as React from "react";` +
+          `import baz2,{yeet}from"c";` +
+          `import baz3,{cats}from"d";` +
+          `import{c,d}from"e";` +
+          `import{e as ee,f as ff}from"f";`
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"import\\"core-js\\";import{foo,bar}from\\"a\\";import baz from\\"b\\";import*as React from\\"react\\";import baz2,{yeet}from\\"c\\";import baz3,{cats}from\\"d\\";import{c,d}from\\"e\\";import{e as ee,f as ff}from\\"f\\""`
+      );
+    });
+
+    const pageFile = 'pages/index.js';
+    const tsPageFile = pageFile.replace(/\.js$/, '.ts');
+
+    test('should not drop unused exports by default in a page', async () => {
+      const code = await swc(
+        // effectful
+        `import"core-js";` +
+          // basic
+          `import{foo,bar}from"a";import baz from"b";` +
+          // complex
+          `import*as React from"react";` +
+          `import baz2,{yeet}from"c";` +
+          `import baz3,{cats}from"d";` +
+          `import{c,d}from"e";` +
+          `import{e as ee,f as ff}from"f";`,
+        { filename: pageFile }
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"import\\"core-js\\";import{foo,bar}from\\"a\\";import baz from\\"b\\";import*as React from\\"react\\";import baz2,{yeet}from\\"c\\";import baz3,{cats}from\\"d\\";import{c,d}from\\"e\\";import{e as ee,f as ff}from\\"f\\""`
+      );
+    });
+
+    test('should support optional chaining for JS file', async () => {
+      const code = await swc(
+        `let hello;` +
+          `export default () => hello?.world ? 'something' : 'nothing' `,
+        {
+          filename: pageFile
+        }
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"var hello;export default function(){return(hello===null||hello===void 0?void 0:hello.world)?'something':'nothing'}"`
+      );
+    });
+
+    test('should support optional chaining for TS file', async () => {
+      const code = await swc(
+        `let hello;` +
+          `export default () => hello?.world ? 'something' : 'nothing' `,
+        {
+          filename: tsPageFile
+        }
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"var hello;export default function(){return(hello===null||hello===void 0?void 0:hello.world)?'something':'nothing'}"`
+      );
+    });
+
+    test('should support nullish coalescing for JS file', async () => {
+      const code = await swc(
+        `const res = {
+          status: 0,
+          nullVal: null,
+          statusText: '',
+  
+        }
+        const status = res.status ?? 999
+        const nullVal = res.nullVal ?? 'another'
+        const statusText = res.nullVal ?? 'not found'
+        export default () => 'hello'
+        `,
+        {
+          filename: pageFile
+        }
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"var res={status:0,nullVal:null,statusText:''};var _status;var status=(_status=res.status)!==null&&_status!==void 0?_status:999;var _nullVal;var nullVal=(_nullVal=res.nullVal)!==null&&_nullVal!==void 0?_nullVal:'another';var _nullVal1;var statusText=(_nullVal1=res.nullVal)!==null&&_nullVal1!==void 0?_nullVal1:'not found';export default function(){return'hello'}"`
+      );
+    });
+
+    test('should support nullish coalescing for TS file', async () => {
+      const code = await swc(
+        `const res = {
+          status: 0,
+          nullVal: null,
+          statusText: '',
+  
+        }
+        const status = res.status ?? 999
+        const nullVal = res.nullVal ?? 'another'
+        const statusText = res.nullVal ?? 'not found'
+        export default () => 'hello'
+        `,
+        {
+          filename: tsPageFile
+        }
+      );
+      expect(code).toMatchInlineSnapshot(
+        `"var res={status:0,nullVal:null,statusText:''};var _status;var status=(_status=res.status)!==null&&_status!==void 0?_status:999;var _nullVal;var nullVal=(_nullVal=res.nullVal)!==null&&_nullVal!==void 0?_nullVal:'another';var _nullVal1;var statusText=(_nullVal1=res.nullVal)!==null&&_nullVal1!==void 0?_nullVal1:'not found';export default function(){return'hello'}"`
+      );
     });
   });
 });
