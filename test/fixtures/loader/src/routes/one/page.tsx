eb27160e1395f0480bd0fb32a56586595937c5b2
Li Qiang <Ives7@users.noreply.github.com>
2022-07-15 18:58:10 +0800

    chore: use routes dir

--- test/fixtures/loader/src/routes/one/page.tsx
+++ test/fixtures/loader/src/routes/one/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData, Link, Loader } from '@shuvi/runtime';
-import { sleep } from '../utils';
+import { sleep } from '../../utils';
 
 const One = () => {
   const data = useLoaderData<LoaderData>();
