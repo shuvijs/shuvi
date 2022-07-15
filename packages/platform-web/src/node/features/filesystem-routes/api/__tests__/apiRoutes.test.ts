db99cccd772cd7f596c94f5ee0eb61b258521a6b
Li Qiang <Ives7@users.noreply.github.com>
2022-07-15 11:08:16 +0800

    test: fix serializeApiRoutes

--- packages/platform-web/src/node/features/filesystem-routes/api/__tests__/apiRoutes.test.ts
+++ packages/platform-web/src/node/features/filesystem-routes/api/__tests__/apiRoutes.test.ts
@@ -19,16 +19,16 @@ describe('serializeApiRoutes', () => {
     expect(serializeApiRoutes(routes)).toMatchInlineSnapshot(`
       "[
       {
-            path: \\"/nested/a\\",
-            api: require(\\"Bar\\"),
-          },
-      {
             path: \\"/\\",
             api: require(\\"Foo\\"),
           },
       {
             path: \\"/nested\\",
             api: require(\\"Foo\\"),
+          },
+      {
+            path: \\"/nested/a\\",
+            api: require(\\"Bar\\"),
           },]"
     `);
   });
