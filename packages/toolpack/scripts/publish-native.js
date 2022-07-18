17b733b3fd0241911c6a854d43746970fb40201e
jinyangwang <214997474@qq.com>
2022-07-18 12:35:39 +0800

    chore: update swc version

--- packages/toolpack/scripts/publish-native.js
+++ packages/toolpack/scripts/publish-native.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const { copy, readFile, readdir, writeFile } = require('fs');
+const { cpSync, readFileSync, readdirSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
-(async function () {
+(function () {
   try {
     const version = require('./version').version;
 
     // Copy binaries to package folders, update version, and publish
     let binarySourcesDir = path.join(__dirname, '../swc-source/native');
     let nativePackagesDir = path.join(__dirname, '../src/swc/npm');
-    let binaryNames = await readdir(binarySourcesDir);
+    let binaryNames = readdirSync(binarySourcesDir);
     console.log(binaryNames);
 
     for (let binaryName of binaryNames) {
@@ -22,25 +22,25 @@ const { execSync } = require('child_process');
         if (!platform) {
           continue;
         }
-        await copy(
+        cpSync(
           path.join(binarySourcesDir, binaryName),
           path.join(nativePackagesDir, platform, binaryName)
         );
         let pkg = JSON.parse(
-          await readFile(path.join(nativePackagesDir, platform, 'package.json'))
+          readFileSync(path.join(nativePackagesDir, platform, 'package.json'))
         );
         pkg.version = version;
-        await writeFile(
+        writeFileSync(
           path.join(nativePackagesDir, platform, 'package.json'),
           JSON.stringify(pkg, null, 2)
         );
-        await execSync(
+        execSync(
           `npm publish ${path.join(
             nativePackagesDir,
             platform
           )} --access public`
         );
-        await execSync(
+        execSync(
           `git update-index --skip-worktree ${path.join(
             nativePackagesDir,
             platform,
