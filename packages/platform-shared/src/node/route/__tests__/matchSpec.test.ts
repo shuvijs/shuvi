5a1deaf59bedb13af4e8d9407636722c7d9fd55f
liximomo <liximomo@gmail.com>
2022-07-19 21:45:35 +0800

    improve: implement exclude according to the behavior of typescript

---
+++ packages/platform-shared/src/node/route/__tests__/matchSpec.test.ts
@@ -0,0 +1,53 @@
+import { matchFile as _matchFile, getFileMatcherPatterns } from '../matchSpec';
+
+function matchFile(file: string, exclude: string[] = []) {
+  const patterns = getFileMatcherPatterns('/', exclude);
+  return _matchFile(file, patterns, false);
+}
+
+describe('routes/matchSpec', () => {
+  test('should ignore node_modules', () => {
+    expect(matchFile('/node_modules')).toBe(false);
+    expect(matchFile('/path/to/node_modules')).toBe(false);
+  });
+
+  test('foo should ignore foo and all sub directories', () => {
+    const test = (file: string) => matchFile(file, ['foo']);
+    expect(test('/foo')).toBe(true);
+    expect(test('/foo/')).toBe(false);
+    expect(test('/foo/a')).toBe(false);
+    expect(test('/foo/a/b')).toBe(false);
+  });
+
+  test('foo should ignore foo.js', () => {
+    const test = (file: string) => matchFile(file, ['foo']);
+    expect(test('/foo')).toBe(true);
+    expect(test('/foo/')).toBe(false);
+    expect(test('/foo/a')).toBe(false);
+    expect(test('/foo/a/b')).toBe(false);
+  });
+
+  test('foo/* should ignore foo and all sub directories', () => {
+    const test = (file: string) => matchFile(file, ['foo/*']);
+    expect(test('/foo')).toBe(true);
+    expect(test('/foo/')).toBe(false);
+    expect(test('/foo/a')).toBe(false);
+    expect(test('/foo/a/b')).toBe(false);
+  });
+
+  test('** should match all sub directories (dir)', () => {
+    const test = (file: string) => matchFile(file, ['foo/**/bar']);
+    expect(test('/foo/zoo')).toBe(true);
+    expect(test('/foo/bar/')).toBe(false);
+    expect(test('/foo/a/bar/')).toBe(false);
+    expect(test('/foo/a/b/bar/')).toBe(false);
+  });
+
+  test('** should match all sub directories (file)', () => {
+    const test = (file: string) => matchFile(file, ['foo/**/bar.js']);
+    expect(test('/foo/zoo')).toBe(true);
+    expect(test('/foo/bar.js')).toBe(false);
+    expect(test('/foo/a/bar.js')).toBe(false);
+    expect(test('/foo/a/b/bar.js')).toBe(false);
+  });
+});
