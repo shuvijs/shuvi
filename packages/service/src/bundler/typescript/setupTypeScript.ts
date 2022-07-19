84977ea2490a530f81e50964cbbd75f4f502c356
myron <myron@gmail.com>
2022-07-19 11:47:01 +0800

    fix: only show typescript setup log when it's necessary

--- packages/service/src/bundler/typescript/setupTypeScript.ts
+++ packages/service/src/bundler/typescript/setupTypeScript.ts
@@ -7,8 +7,9 @@ import { writeDefaultConfigurations } from './configTypeScript';
 import {
   hasTsConfig,
   hasTypescriptFiles,
-  getTypeScriptPath,
-  getTsConfig
+  checkNecessarytDeps,
+  getTsConfig,
+  PackageDep
 } from './getTypeScriptInfo';
 
 let hasSetup = false;
@@ -26,6 +27,45 @@ let typeScriptPath: string | undefined;
 let tsConfigPath: string | undefined;
 let tsCompilerOptions: TsCompilerOptions;
 let resolvedBaseUrl: string | undefined;
+
+function printMissingPackagesError(pkgs: PackageDep[]) {
+  const packagesHuman = pkgs
+    .map(
+      (p, index, { length }) =>
+        (index > 0
+          ? index === length - 1
+            ? length > 2
+              ? ', and '
+              : ' and '
+            : ', '
+          : '') + p.pkg
+    )
+    .join('');
+  const packagesCli = pkgs.map(p => p.pkg).join(' ');
+
+  console.error(
+    chalk.bold.red(
+      `It looks like you're trying to use TypeScript but do not have the required package(s) installed.`
+    )
+  );
+  console.error();
+  console.error(
+    chalk.bold(`Please install ${chalk.bold(packagesHuman)} by running:`)
+  );
+  console.error();
+  console.error(
+    `\t${chalk.bold.cyan('npm install --save-dev' + ' ' + packagesCli)}`
+  );
+  console.error();
+  console.error(
+    chalk.bold(
+      'If you are not trying to use TypeScript, please remove the ' +
+        chalk.cyan('tsconfig.json') +
+        ' file from your package root (and any TypeScript files).'
+    )
+  );
+  console.error();
+}
 
 export function getTypeScriptInfo(): TypeScriptInfo {
   if (!hasSetup) {
@@ -55,17 +95,18 @@ export async function setupTypeScript(paths: IPaths) {
   useTypeScript = await hasTypescriptFiles(paths.srcDir);
   tsCompilerOptions = {};
   if (useTypeScript) {
-    typeScriptPath = getTypeScriptPath(projectDir);
-    if (!typeScriptPath) {
+    const deps = checkNecessarytDeps(projectDir);
+    if (deps.missing.length > 0) {
+      printMissingPackagesError(deps.missing);
       process.exit(-1);
     }
 
+    typeScriptPath = deps.resovled.get('typescript');
     tsConfigPath = path.join(projectDir, 'tsconfig.json');
     const needDefaultTsConfig = !(await hasTsConfig(tsConfigPath));
-
     if (needDefaultTsConfig) {
       console.log(
-        chalk.yellow(
+        chalk.cyan(
           `We detected TypeScript in your project and created a ${chalk.bold(
             'tsconfig.json'
           )} file for you.`
@@ -78,7 +119,13 @@ export async function setupTypeScript(paths: IPaths) {
     const { default: ts } = (await import(typeScriptPath!)) as TypeScriptModule;
     const tsConfig = await getTsConfig(ts, tsConfigPath);
     tsCompilerOptions = tsConfig.options;
-    await writeDefaultConfigurations(ts, tsConfigPath, tsConfig, paths);
+    await writeDefaultConfigurations(
+      ts,
+      tsConfigPath,
+      tsConfig,
+      paths,
+      needDefaultTsConfig
+    );
   }
 
   const jsConfigPath = path.join(projectDir, 'jsconfig.json');
