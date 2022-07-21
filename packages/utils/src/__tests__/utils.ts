00f7a9dc2c4248a4f944de2082b5310d1abc2edf
liximomo <liximomo@gmail.com>
2022-07-21 16:12:11 +0800

    chore: replace unix-only cmd

--- packages/utils/src/__tests__/utils.ts
+++ packages/utils/src/__tests__/utils.ts
@@ -1,14 +1,14 @@
 import * as path from 'path';
-import { execSync } from 'child_process';
+import fse from 'fs-extra';
 
 export function resolveFixture(name: string) {
   return path.join(__dirname, 'fixtures', name);
 }
 
 export function copyDirectory(source: string, target: string) {
-  execSync(`cp -R ${source} ${target}`);
+  fse.copySync(source, target);
 }
 
 export function deleteDirectory(source: string) {
-  execSync(`rm -rf ${source}`);
+  fse.removeSync(source);
 }
