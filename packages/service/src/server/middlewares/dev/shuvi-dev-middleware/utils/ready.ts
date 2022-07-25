f388230d472aa2d691865bb29317878324898699
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-07-25 14:01:22 +0800

    refactor: merge into devMiddleware

--- packages/service/src/server/middlewares/dev/shuvi-dev-middleware/utils/ready.ts
+++
@@ -1,8 +0,0 @@
-import { IContext, ICallback } from '../types';
-
-export default function ready(context: IContext, callback: ICallback) {
-  if (context.state) {
-    return callback(context.stats);
-  }
-  context.callbacks.push(callback);
-}
