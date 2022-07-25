8039a3e44a8672e6827506ecdd97c5be761f3e38
myron <myron@gmail.com>
2022-07-25 19:48:21 +0800

    refactor: fix node.js types error

--- packages/service/src/server/shuviProdServer.ts
+++ packages/service/src/server/shuviProdServer.ts
@@ -3,10 +3,8 @@ import { getAssetMiddleware } from './middlewares/getAssetMiddleware';
 export class ShuviProdServer extends ShuviServer {
   async init() {
     const { _serverContext: context } = this;
-    if (context.assetPublicPath.startsWith('/')) {
-      const assetsMiddleware = getAssetMiddleware(context);
-      this._server.use(`${context.assetPublicPath}:path(.*)`, assetsMiddleware);
-    }
+    const assetsMiddleware = getAssetMiddleware(context);
+    this._server.use(`${context.assetPublicPath}:path(.*)`, assetsMiddleware);
     await this._initMiddlewares();
   }
 }
