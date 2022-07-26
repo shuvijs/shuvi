28f495ba8e0b7ad5ccba3779f198a4f4f9555389
Yu-An Chang <andy.yuanchang@gmail.com>
2022-07-26 09:27:16 +0800

    chore: move CustomCorePluginHooks from shuvi/runtime to shuvi/service
    Co-authored-by: X.L <liximomo@gmail.com>

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -2,8 +2,6 @@ import { HookMap } from '@shuvi/hook';
 
 export interface CustomAppContext {}
 
-export interface CustomCorePluginHooks extends HookMap {}
-
 export interface CustomServerPluginHooks extends HookMap {}
 
 export interface CustomRuntimePluginHooks extends HookMap {}
