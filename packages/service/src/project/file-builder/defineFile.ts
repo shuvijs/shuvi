9d07708856cecd0ef2147089556bc855b65e9af7
Repraance <reprance@gmail.com>
2022-07-26 10:27:34 +0800

    feat(fileBuilder): batch update
    Co-authored-by: X.L <liximomo@gmail.com>

---
+++ packages/service/src/project/file-builder/defineFile.ts
@@ -0,0 +1,11 @@
+import { uuid } from './utils';
+import type { FileOptionWithoutId, DefineFile } from './types';
+
+export const defineFile: DefineFile = <T = string, C = any>(
+  fileOption: FileOptionWithoutId<T, C>
+) => {
+  return {
+    ...fileOption,
+    id: uuid()
+  };
+};
