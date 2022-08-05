35426ab58df1c3a96dd512338edec17b56416403
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-08-05 19:23:45 +0800

    refactor: remove tab function

--- packages/error-overlay/src/react/components/Overlay/Overlay.tsx
+++ packages/error-overlay/src/react/components/Overlay/Overlay.tsx
@@ -1,5 +1,3 @@
-// @ts-ignore
-import allyTrap from './maintain--tab-focus';
 import * as React from 'react';
 import { lock, unlock } from './body-locker';
 
@@ -21,24 +19,8 @@ const Overlay: React.FC<OverlayProps> = function Overlay({
     };
   }, []);
 
-  const [overlay, setOverlay] = React.useState<HTMLDivElement | null>(null);
-  const onOverlay = React.useCallback((el: HTMLDivElement) => {
-    setOverlay(el);
-  }, []);
-
-  React.useEffect(() => {
-    if (overlay == null) {
-      return;
-    }
-
-    const handle2 = allyTrap({ context: overlay });
-    return () => {
-      handle2.disengage();
-    };
-  }, [overlay]);
-
   return (
-    <div dialog-overlay className={className} ref={onOverlay}>
+    <div dialog-overlay className={className}>
       <div dialog-backdrop dialog-backdrop-fixed={fixed ? true : undefined} />
       {children}
     </div>
