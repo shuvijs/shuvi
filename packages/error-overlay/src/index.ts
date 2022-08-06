81fdf64484b32b8c5f677fb305f994e3c86b9e4a
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-08-06 23:03:10 +0800

    refactor: export from index

--- packages/error-overlay/src/index.ts
+++ packages/error-overlay/src/index.ts
@@ -1,2 +1,4 @@
 // This package 'error-overlay' is a modified version of the Next.js that can be found here:
 // https://github.com/vercel/next.js/blob/canary/packages/react-dev-overlay/
+
+export { default as ReactDevOverlay } from './react/ReactDevOverlay';
