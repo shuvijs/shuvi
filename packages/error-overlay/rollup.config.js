5d15d9f4f827143b00ed5cc49d65738b9542f0a1
ives <mine@ives.pro>
2022-08-11 11:25:39 +0800

    增加rollup replace插件

--- packages/error-overlay/rollup.config.js
+++ packages/error-overlay/rollup.config.js
@@ -1,30 +1,32 @@
 import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import ts from 'rollup-plugin-typescript2';
 
-const extensions = ['.js', '.ts', '.tsx','jsx'];
+const extensions = ['.js', '.ts', '.tsx', 'jsx'];
 
 export default {
-
   input: path.join(__dirname, 'src/index.ts'),
 
   plugins: [
-
-    commonjs({
-
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
     }),
+    commonjs(),
     resolve({ extensions }),
 
     ts({
       tsconfig: path.join('./tsconfig.build.json'),
       extensions
-    }),
-
+    })
   ],
   output: {
     name: 'ErrorOverlay',
     file: path.join(__dirname, './umd/index.js'),
-    format: 'umd',
-  },
+    format: 'umd'
+  }
 };
