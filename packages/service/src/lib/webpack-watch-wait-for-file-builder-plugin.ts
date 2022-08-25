1b1bc7d071502f4730314bb0ef5dd3bb65af5b13
Repraance <reprance@gmail.com>
2022-08-25 20:27:17 +0800

    fix: make webpack.compile wait until fileBuilder is finished

---
+++ packages/service/src/lib/webpack-watch-wait-for-file-builder-plugin.ts
@@ -0,0 +1,119 @@
+import { Compiler, Plugin } from '@shuvi/toolpack/lib/webpack';
+import { ProjectBuilder } from '../project/projectBuilder';
+import { FileInfo } from '../project/index';
+
+const checkResumeInterval = 10;
+const fallbackTimeout = 1000 * 5;
+
+type Options = {
+  onBuildStart: ProjectBuilder['onBuildStart'];
+  onBuildEnd: ProjectBuilder['onBuildEnd'];
+  onInvalid: ProjectBuilder['onInvalid'];
+  isDependency: ProjectBuilder['isDependency'];
+};
+
+const mergeMaps = <K, V>(remaining: Map<K, V>, dropped: ReadonlyMap<K, V>) => {
+  for (const [file, info] of dropped) {
+    remaining.set(file, info);
+  }
+};
+
+export default class WebpackWatchWaitForFileBuilderPlugin implements Plugin {
+  options: Options;
+
+  constructor(options: Options) {
+    this.options = options;
+  }
+  apply(compiler: Compiler) {
+    const { onBuildEnd, onInvalid, isDependency } = this.options;
+    /**
+     * watching.suspend will pause the real action in the watcher handler but still collecting changed files.
+     * watching.resume will resume its action
+     * And resume when onBuildEnd.
+     * In this way, during build of fileBuilder, webpack will not trigger any watchRun event but keep watching changed files.
+     */
+    let checkResumeIntervalTimer: NodeJS.Timer | undefined;
+    let fallbackTimer: NodeJS.Timer | undefined;
+    let collectedChangedFiles = new Map<string, FileInfo>();
+    onInvalid(() => {
+      compiler.watching.suspend();
+      if (checkResumeIntervalTimer) {
+        clearInterval(checkResumeIntervalTimer);
+      }
+    });
+
+    /**
+     * check if webpack watching can resume.
+     * webpack watching cannot resume until all the changed files has been detected by webpack watcher.
+     *
+     */
+    const canResume = (changedFiles: ReadonlyMap<string, FileInfo>) => {
+      const fileInfoEntries =
+        compiler.watching.watcher?.getInfo?.().fileTimeInfoEntries;
+      if (!fileInfoEntries) return false;
+
+      for (const [file, { timestamp }] of changedFiles) {
+        const fileInfo = fileInfoEntries.get(file);
+        const safeTime: number = (fileInfo as any)?.safeTime || 0;
+
+        // webpack watcher's safeTime should >= timestamp
+        if (safeTime < timestamp) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    onBuildEnd(({ changedFiles }) => {
+      mergeMaps(collectedChangedFiles, changedFiles);
+
+      // fileBuilder's files have changed, wait webpack watcher until it also detect these files have changed
+      if (collectedChangedFiles.size) {
+        if (!checkResumeIntervalTimer) {
+          checkResumeIntervalTimer = setInterval(() => {
+            if (canResume(collectedChangedFiles)) {
+              collectedChangedFiles.clear();
+              compiler.watching.resume();
+              clearInterval(checkResumeIntervalTimer);
+              clearTimeout(fallbackTimer);
+              checkResumeIntervalTimer = undefined;
+            }
+          }, checkResumeInterval);
+        }
+
+        clearTimeout(fallbackTimer);
+        // set a fallback timer in case of an exception that cannot be resumed
+        fallbackTimer = setTimeout(() => {
+          collectedChangedFiles.clear();
+          compiler.watching.resume();
+          clearInterval(checkResumeIntervalTimer);
+          checkResumeIntervalTimer = undefined;
+        }, fallbackTimeout);
+      } else {
+        // resume directly when no changed files
+        compiler.watching.resume();
+      }
+    });
+
+    compiler.hooks.invalid.tap(
+      'WebpackWatchWaitForFileBuilderPlugin-invalid',
+      file => {
+        // collect changed files and removed files and check if they are the dependencies of the fileBuilder
+        // if yes, invoke `compiler.watching.suspend()`
+        const removedFiles: Set<string> | undefined = (compiler.watching as any)
+          ._collectedRemovedFiles;
+        const files = new Set(removedFiles);
+        if (file) {
+          files.add(file);
+        }
+
+        for (const currentFile of files) {
+          if (isDependency(currentFile)) {
+            compiler.watching.suspend();
+            return;
+          }
+        }
+      }
+    );
+  }
+}
--- packages/service/src/project/file-builder/types.ts
+++ packages/service/src/project/file-builder/types.ts
@@ -22,18 +22,12 @@ export type FileOptionWithoutId<T = string, C = any> = Omit<
 
 export interface FileInternalInstance<T = string, C = any> {
   name?: string;
+  fullPath?: string;
   id: FileId;
   virtual?: boolean;
   content: ContentFunction<T, C>;
   fileContent?: T;
 }
-
-/* export interface DefineFile {
-  <T = string, C = any>(fileOption: FileOptionWithoutId<T, C>): FileOption<
-    T,
-    C
-  >;
-} */
 
 export type DefineFile = <T = string, C = any>(
   fileOption: FileOptionWithoutId<T, C>
@@ -44,19 +38,57 @@ export type DependencyInfo = {
   dependents: Set<FileId>;
 };
 
+export type FileInfo = {
+  /** timestamp that the file changes */
+  timestamp: number;
+};
+
+/**
+ * Infos for a buildOnce
+ */
 export type BuildInfo = {
+  /**
+   * uuid of this buildOnce
+   */
   id: string;
-  files: Set<FileId>;
+  /**
+   * All files included in this build. Will never change
+   */
+  files: ReadonlySet<FileId>;
+  /**
+   * The set of ids of the buildOnces which are in front of this buildOnce
+   */
   fronts: Set<string>;
+  /**
+   * The set of ids of the buildOnces which are in rear of this buildOnce
+   */
   rears: Set<string>;
+  /**
+   * changed files that includes all the changed files of all the fronts.
+   */
+  collectedChangedFiles: Map<FileId, FileInfo>;
 };
 
 export type FileStatus = {
+  /** whether this file has been built*/
   updated: boolean;
-  noChange?: boolean;
+  /** whether this file has changed after build */
+  changed: boolean;
+  changedTime?: number;
+};
+
+export type BuildRunningInfo = {
+  filesStatusMap: Map<FileId, FileStatus>;
+  /**
+   * Files waiting to be built in this build. Will change until be cleared.
+   */
+  pendingFiles: Set<FileId>;
 };
 
 export type FilesInfo = {
   filesStatusMap: Map<FileId, FileStatus>;
-  files: Set<FileId>;
+  /**
+   * Files waiting to be built in this build. Will change until be cleared.
+   */
+  pendingFiles: Set<FileId>;
 };
