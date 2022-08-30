022b33c86bf7b4f7051dce21e5a42778d3579b68
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-08-30 09:25:15 +0800

    chore: update pnpm lock

--- packages/create-shvui-app/templates/default/shuvi.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'shuvi';
-
-export default defineConfig({
-  ssr: true
-});
--- packages/create-shvui-app/templates/typescript/src/routes/api/api.ts
+++
@@ -1,6 +0,0 @@
-export default (req, res) => {
-  if (req.method === 'POST') {
-    return res.status(200).json(req.body);
-  }
-  return res.send('Hello World!');
-};
