f42002709c2cea5984473258d9624fdf6cc314f1
liximomo <liximomo@gmail.com>
2022-08-30 18:14:41 +0800

    optimize: reduce style FOUC

--- packages/platform-web/src/shuvi-app/entry/client/run.dev.ts
+++ packages/platform-web/src/shuvi-app/entry/client/run.dev.ts
@@ -1,25 +1,21 @@
 /// <reference lib="dom" />
-import {
-  DEV_STYLE_HIDE_FOUC,
-  DEV_STYLE_PREPARE
-} from '@shuvi/shared/lib/constants';
+import { DEV_STYLE_HIDE_FOUC } from '@shuvi/shared/lib/constants';
 import { initHMRAndDevClient } from '../../dev';
 import { run, app } from './app';
 
-async function init() {
-  initHMRAndDevClient(app);
-  // reduce FOUC caused by style-loader
-  const styleReady = new Promise<void>(resolve => {
-    (window.requestAnimationFrame || setTimeout)(async () => {
-      await (window as any)[DEV_STYLE_PREPARE];
-      document
-        .querySelectorAll(`[${DEV_STYLE_HIDE_FOUC}]`)
-        .forEach(el => el.parentElement?.removeChild(el));
-      resolve();
-    });
-  });
-
-  await styleReady!;
+function applyStyle() {
+  document
+    .querySelectorAll(`[${DEV_STYLE_HIDE_FOUC}]`)
+    .forEach(el => el.parentElement?.removeChild(el));
 }
 
-init().then(() => run());
+async function init() {
+  initHMRAndDevClient(app);
+}
+
+init()
+  .then(() => run())
+  .finally(() => {
+    // reduce FOUC caused by style-loader
+    applyStyle();
+  });
