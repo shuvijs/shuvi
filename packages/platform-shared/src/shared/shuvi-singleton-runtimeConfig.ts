6862e39521f531b9d24ca256a5c75e5e4eb3d56e
liximomo <liximomo@gmail.com>
2022-09-13 16:13:48 +0800

    feat: bundle runtimeConfig

--- packages/platform-shared/src/shared/shuvi-singleton-runtimeConfig.ts
+++ packages/platform-shared/src/shared/shuvi-singleton-runtimeConfig.ts
@@ -1,28 +1,53 @@
 import { IRuntimeConfig } from './runtimeConfigTypes';
 
-let runtimeConfig: IRuntimeConfig | null;
-let publicRuntimeConfig: IRuntimeConfig | null;
+const KEY_SERVER_RUNTIME_CONFIG = Symbol.for('shuvi_server_runtime_config');
+const KEY_PUBLIC_RUNTIME_CONFIG = Symbol.for('shuvi_client_runtime_config');
+
+const isServer = typeof window === 'undefined';
+
+let publicRuntimeConfig: IRuntimeConfig | undefined | null;
+let serverRuntimeConfig: IRuntimeConfig | undefined | null;
 
 /**
  * getRuntimeConfig function
  *
- * @returns runtimeConfig
+ * @returns serverRuntimeConfig
  */
 export function getRuntimeConfig() {
   return {
-    ...(runtimeConfig || {}),
-    ...(publicRuntimeConfig || {})
+    ...(getServerRuntimeConfig() || {}),
+    ...(getPublicRuntimeConfig() || {})
   };
 }
 
-export function getPublicRuntimeConfig(): IRuntimeConfig | null {
-  return publicRuntimeConfig;
+export function getPublicRuntimeConfig(): IRuntimeConfig | undefined | null {
+  if (isServer) {
+    return (globalThis as any)[KEY_PUBLIC_RUNTIME_CONFIG];
+  } else {
+    return publicRuntimeConfig;
+  }
 }
 
-export function setRuntimeConfig(config: IRuntimeConfig) {
-  runtimeConfig = config;
+export function setPublicRuntimeConfig(config: IRuntimeConfig | null) {
+  if (isServer) {
+    (globalThis as any)[KEY_PUBLIC_RUNTIME_CONFIG] = config;
+  } else {
+    publicRuntimeConfig = config;
+  }
 }
 
-export function setPublicRuntimeConfig(config: IRuntimeConfig) {
-  publicRuntimeConfig = config;
+export function getServerRuntimeConfig(): IRuntimeConfig | undefined | null {
+  if (isServer) {
+    return (globalThis as any)[KEY_SERVER_RUNTIME_CONFIG];
+  } else {
+    return serverRuntimeConfig;
+  }
+}
+
+export function setServerRuntimeConfig(config: IRuntimeConfig | null) {
+  if (isServer) {
+    (globalThis as any)[KEY_SERVER_RUNTIME_CONFIG] = config;
+  } else {
+    serverRuntimeConfig = config;
+  }
 }
