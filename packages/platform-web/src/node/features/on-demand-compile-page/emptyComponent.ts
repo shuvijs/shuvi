08eb069e8c2ca042145e23b3d96bbef5da1d95f9
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-09-15 15:30:44 +0800

    fix: fix the repeat log

--- packages/platform-web/src/node/features/on-demand-compile-page/emptyComponent.ts
+++ packages/platform-web/src/node/features/on-demand-compile-page/emptyComponent.ts
@@ -1,6 +1,4 @@
-import logger from '@shuvi/utils/lib/logger';
-
 export default function () {
-  logger.warn('You should compile the module before using it.');
+  console.warn('You should compile the module before using it.');
   return null;
 }
