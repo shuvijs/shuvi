f4c717dfedf3bbe0406795c27c67af3e35ce7f21
Tom <21499747@qq.com>
2022-10-19 14:18:25 +0800

    fix(swc)L keep loader remove effects

--- packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/input.js
+++ packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/input.js
@@ -1,2 +1,19 @@
-import './side-effects'
-export { loader, default } from "a";
+import fs from 'fs';
+import other from 'other';
+
+import './side-effects';
+
+var a = [];
+console.log('![a] =>', a);
+a.forEach(b => console.log(b));
+window.c = 'test';
+
+const { p } = other;
+
+export async function loader() {
+  p;
+}
+
+export default function Home() {
+  return <div />;
+}
