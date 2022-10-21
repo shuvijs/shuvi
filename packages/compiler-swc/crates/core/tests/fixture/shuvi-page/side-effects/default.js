8c2c8d609c16b84e4c690014c2b71a8f9c2aa11c
jinyangwang <214997474@qq.com>
2022-10-21 08:56:00 +0800

    fix(swc): pick loader and skip effects

--- packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/default.js
+++ packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/default.js
@@ -1,2 +1,9 @@
-import './side-effects'
-export { default } from "a";
+import fs from 'fs';
+import './side-effects';
+var a = [];
+console.log('![a] =>', a);
+a.forEach(b => console.log(b));
+window.c = 'test';
+export default function Home() {
+  return __jsx('div', null);
+}
