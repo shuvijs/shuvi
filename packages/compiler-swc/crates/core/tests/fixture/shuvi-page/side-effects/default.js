ce28a73a3bf1d85d4fde36fcb00db4fc5627f64c
Tom <21499747@qq.com>
2022-10-21 09:20:53 +0800

    fix(swc): test bug

--- packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/default.js
+++ packages/compiler-swc/crates/core/tests/fixture/shuvi-page/side-effects/default.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import './side-effects';
 var a = [];
 console.log('![a] =>', a);
-a.forEach(b => console.log(b));
+a.forEach((b)=>console.log(b));
 window.c = 'test';
 export default function Home() {
-  return __jsx('div', null);
-}
+    return __jsx("div", null);
+};
