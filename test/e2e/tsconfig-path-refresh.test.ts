594d81517f1d2fa47619086c5d8ff21cd654504d
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-10-25 16:27:15 +0800

    feat: add e2e test for paths updated/added in tsconfig file

--- test/e2e/tsconfig-path-refresh.test.ts
+++ test/e2e/tsconfig-path-refresh.test.ts
@@ -1,8 +1,17 @@
-import { AppCtx, Page, serveFixture } from '../utils/index';
+import {
+  AppCtx,
+  Page,
+  devFixture,
+  resolveFixture,
+  check
+} from '../utils/index';
+import { readFileSync, writeFileSync } from 'fs';
 
 jest.setTimeout(5 * 60 * 1000);
 
 const FIXTURE = 'tsconfig-path-refresh';
+const TSCONFIG_FILE_PATH = `${FIXTURE}/tsconfig.json`;
+const MAIN_PAGE_PATH = `${FIXTURE}/src/routes/page.tsx`;
 
 declare global {
   interface Window {
@@ -10,11 +19,12 @@ declare global {
   }
 }
 
-let ctx: AppCtx;
-let page: Page;
 describe('Tsconfig Path Refresh', () => {
+  let ctx: AppCtx;
+  let page: Page;
+
   beforeAll(async () => {
-    ctx = await serveFixture(FIXTURE);
+    ctx = await devFixture(FIXTURE);
     page = await ctx.browser.page(ctx.url('/'));
   });
 
@@ -31,5 +41,151 @@ describe('Tsconfig Path Refresh', () => {
         data: 'first'
       })
     );
+  });
+
+  test('should recover from module not found and automatically load updated paths correctly', async () => {
+    const mainPageContent = readFileSync(
+      resolveFixture(MAIN_PAGE_PATH),
+      'utf8'
+    );
+    const tsconfigContent = readFileSync(
+      resolveFixture(TSCONFIG_FILE_PATH),
+      'utf8'
+    );
+    try {
+      expect(await page.$text('#first-component')).toEqual('first component');
+      expect(await page.$text('#second-component')).toEqual('second component');
+      expect(await page.$text('#first-data')).toEqual(
+        JSON.stringify({
+          data: 'first'
+        })
+      );
+      let secondData;
+      try {
+        secondData = await page.$text('#second-data');
+      } catch {
+        secondData = null;
+      }
+      // secondData not found
+      expect(secondData).toEqual(null);
+
+      // edit the main page import and content
+      let editedMainPageContent = mainPageContent.replace(
+        "import { firstData } from '@lib/first-data'",
+        `import { firstData } from '@lib/first-data';
+      import { secondData } from '@lib/second-data'`
+      );
+      editedMainPageContent = editedMainPageContent.replace(
+        '<p id="first-data">{JSON.stringify(firstData)}</p>',
+        `<p id="first-data">{JSON.stringify(firstData)}</p>
+      <p id="second-data">{JSON.stringify(secondData)}</p>`
+      );
+      writeFileSync(
+        resolveFixture(MAIN_PAGE_PATH),
+        editedMainPageContent,
+        'utf8'
+      );
+
+      // edit the tsconfig paths
+      let editedTsconfigContent = tsconfigContent.replace(
+        '["src/lib/first-lib/*"]',
+        '["src/lib/first-lib/*", "src/lib/second-lib/*"]'
+      );
+      writeFileSync(
+        resolveFixture(TSCONFIG_FILE_PATH),
+        editedTsconfigContent,
+        'utf8'
+      );
+
+      expect(await page.$text('#first-component')).toEqual('first component');
+      expect(await page.$text('#second-component')).toEqual('second component');
+      expect(await page.$text('#first-data')).toEqual(
+        JSON.stringify({
+          data: 'first'
+        })
+      );
+      // secondData should be found
+      await check(
+        () => page.$text('#second-data'),
+        t =>
+          t ===
+          JSON.stringify({
+            data: 'second'
+          })
+      );
+    } finally {
+      writeFileSync(resolveFixture(MAIN_PAGE_PATH), mainPageContent, 'utf8');
+      writeFileSync(
+        resolveFixture(TSCONFIG_FILE_PATH),
+        tsconfigContent,
+        'utf8'
+      );
+    }
+  });
+
+  test('should automatically load added paths correctly', async () => {
+    const mainPageContent = readFileSync(
+      resolveFixture(MAIN_PAGE_PATH),
+      'utf8'
+    );
+    const tsconfigContent = readFileSync(
+      resolveFixture(TSCONFIG_FILE_PATH),
+      'utf8'
+    );
+    try {
+      expect(await page.$text('#first-component')).toEqual('first component');
+      expect(await page.$text('#second-component')).toEqual('second component');
+      expect(await page.$text('#first-data')).toEqual(
+        JSON.stringify({
+          data: 'first'
+        })
+      );
+
+      // edit the main page import
+      let editedMainPageContent = mainPageContent.replace(
+        '@myComponent2',
+        '@myTestComponent2'
+      );
+      writeFileSync(
+        resolveFixture(MAIN_PAGE_PATH),
+        editedMainPageContent,
+        'utf8'
+      );
+
+      // edit the tsconfig paths
+      const tsconfig = JSON.parse(tsconfigContent);
+      writeFileSync(
+        resolveFixture(TSCONFIG_FILE_PATH),
+        JSON.stringify({
+          ...tsconfig,
+          compilerOptions: {
+            paths: {
+              ...tsconfig.compilerOptions.paths,
+              '@myTestComponent2': ['src/components/component-2-test.tsx']
+            }
+          }
+        }),
+        'utf8'
+      );
+
+      expect(await page.$text('#first-component')).toEqual('first component');
+      // component-2 should be updated to component-2-test
+      await check(
+        () => page.$text('#second-component'),
+        t => /Test 2 component/.test(t)
+      );
+      expect(await page.$text('#first-data')).toEqual(
+        JSON.stringify({
+          data: 'first'
+        })
+      );
+    } finally {
+      writeFileSync(resolveFixture(MAIN_PAGE_PATH), mainPageContent, 'utf8');
+      writeFileSync(
+        resolveFixture(TSCONFIG_FILE_PATH),
+        tsconfigContent,
+        'utf8'
+      );
+    }
   });
 });
