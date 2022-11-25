cdd93478610c294dd9c16d4eda288ed659da8bdf
Harry <45198715+harrytothemoon@users.noreply.github.com>
2022-11-25 15:47:19 +0800

    chore: add a reference to the next.js comment

--- packages/telemetry/src/helper/project-id.ts
+++ packages/telemetry/src/helper/project-id.ts
@@ -1,16 +1,5 @@
 import { execSync } from 'child_process';
 
-// Q: Why does Next.js need a project ID? Why is it looking at my git remote?
-// A:
-// Next.js' telemetry is and always will be completely anonymous. Because of
-// this, we need a way to differentiate different projects to track feature
-// usage accurately. For example, to prevent a feature from appearing to be
-// constantly `used` and then `unused` when switching between local projects.
-// To reiterate,
-// we **never** can read your actual git remote. The value is hashed one-way
-// with random salt data, making it impossible for us to reverse or try to
-// guess the remote by re-computing hashes.
-
 function _getProjectIdByGit() {
   try {
     const originBuffer = execSync(
--- packages/telemetry/src/index.ts
+++ packages/telemetry/src/index.ts
@@ -1,1 +1,4 @@
+// This package 'telemetry' is a modified version of the Next.js that can be found here:
+// https://github.com/vercel/next.js/tree/canary/packages/next/telemetry
+
 export { Telemetry } from './telemetry';
