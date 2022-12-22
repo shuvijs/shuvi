12f3462ba13ac33c2e35b2052ab6aca1e3e6cd2f
liximomo <liximomo@gmail.com>
2022-12-22 11:48:49 +0800

    chore: remove unused config

--- jest.config.js
+++ jest.config.js
@@ -42,8 +42,6 @@ module.exports = {
   ],
 
   moduleNameMapper: {
-    '^shuvi-test-utils(/?.*)$': '<rootDir>/test/utils/$1',
-    '^@shuvi/plugins/model$': '@shuvi/plugins/lib/model',
-    '^@shuvi/plugins/model/(.*)': '@shuvi/plugins/lib/model/$1'
+    '^shuvi-test-utils(/?.*)$': '<rootDir>/test/utils/$1'
   }
 };
