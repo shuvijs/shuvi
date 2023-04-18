daeffee9bd458dd82548a4b93427c286e3d625c0
Tom <21499747@qq.com>
2023-04-18 22:00:28 +0800

    chore: comment some rules

--- packages/eslint-config-shuvi/index.js
+++ packages/eslint-config-shuvi/index.js
@@ -57,26 +57,33 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:@shuvi/shuvi/recommended'
   ],
-  plugins: ['@shuvi/shuvi', 'import', 'react', 'jsx-a11y'],
+  // plugins: ['@shuvi/shuvi', 'import', 'react', 'jsx-a11y'],
+  plugins: ['@shuvi/shuvi', 'react'],
+  // rules: {
+  //   'import/no-anonymous-default-export': 'warn',
+  //   'react/no-unknown-property': 'off',
+  //   'react/react-in-jsx-scope': 'off',
+  //   'react/prop-types': 'off',
+  //   'react/display-name': 'off',
+  //   'react/jsx-no-target-blank': 'off',
+  //   'jsx-a11y/alt-text': [
+  //     'warn',
+  //     {
+  //       elements: ['img'],
+  //       img: ['Image']
+  //     }
+  //   ],
+  //   'jsx-a11y/aria-props': 'warn',
+  //   'jsx-a11y/aria-proptypes': 'warn',
+  //   'jsx-a11y/aria-unsupported-elements': 'warn',
+  //   'jsx-a11y/role-has-required-aria-props': 'warn',
+  //   'jsx-a11y/role-supports-aria-props': 'warn'
+  // },
   rules: {
-    'import/no-anonymous-default-export': 'warn',
     'react/no-unknown-property': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'react/jsx-no-target-blank': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image']
-      }
-    ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn'
+    'react/jsx-no-target-blank': 'off'
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
