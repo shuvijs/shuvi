09a52b2515ff7da399e8a105341606c76d05f362
Tom <21499747@qq.com>
2023-06-14 16:37:59 +0800

    chore: use @shuvi/reporters

--- packages/shuvi/src/shuvi.ts
+++ packages/shuvi/src/shuvi.ts
@@ -1,10 +1,7 @@
-import { Telemetry } from '@shuvi/telemetry';
 import { IPlatform, getApi, Api, ShuviConfig } from '@shuvi/service';
 import platformWeb from '@shuvi/platform-web';
 import { ShuviMode, ShuviPhase } from './types';
 import { normalizePlatformConfig } from './config';
-//@ts-ignore
-import pkgInfo from '../package.json';
 
 export function getPlatform(): IPlatform {
   return platformWeb({ framework: 'react' });
@@ -18,21 +15,11 @@ export interface ShuviOption {
   configFile?: string | false;
 }
 
-const TELEMETRY_ENDPOINT = '';
-
 export async function initShuvi({
   config,
   ...options
 }: ShuviOption): Promise<Api> {
   const { plugins, presets, ...restConfig } = config;
-
-  const telemetry = TELEMETRY_ENDPOINT
-    ? new Telemetry({
-        name: 'Shuvi.js',
-        meta: { shuviVersion: pkgInfo.version },
-        postEndpoint: TELEMETRY_ENDPOINT
-      })
-    : undefined;
 
   const shuvi = await getApi({
     ...options,
@@ -40,8 +27,7 @@ export async function initShuvi({
     presets,
     config: restConfig,
     platform: getPlatform(),
-    normalizePlatformConfig,
-    telemetry
+    normalizePlatformConfig
   });
 
   return shuvi;
