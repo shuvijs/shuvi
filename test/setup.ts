8598cd00b9ab47c02814b33c9bb31e9ba1f6d416
Tom <21499747@qq.com>
2023-06-21 17:48:05 +0800

    chore: ignore warning

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,6 @@
 const OLD_ENVS = { ...process.env };
-
+process.env.BROWSERSLIST_IGNORE_OLD_DATA = 'true';
 beforeEach(() => {
   process.env = { ...OLD_ENVS };
+  process.env.BROWSERSLIST_IGNORE_OLD_DATA = 'true';
 });
