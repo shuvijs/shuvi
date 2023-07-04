c934ad6f15a5e2f15a3a762b7f2ec13c5648c281
jinyangwang <214997474@qq.com>
2023-07-04 11:07:03 +0800

    fix: make cicd stable

--- packages/platform-web/src/shuvi-app/react/__tests__/dynamic.test.tsx
+++ packages/platform-web/src/shuvi-app/react/__tests__/dynamic.test.tsx
@@ -4,7 +4,7 @@
 
 import * as React from 'react';
 import { getByText, render, waitFor, cleanup } from '@testing-library/react';
-import { wait } from 'shuvi-test-utils';
+import { wait } from 'shuvi-test-utils/shared';
 import dynamic from '../dynamic';
 
 describe('dynamic', () => {
