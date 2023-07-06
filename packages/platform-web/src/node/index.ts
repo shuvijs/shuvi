3ea6e6eaaf3103df97c5de300ffa3c9e9773616b
Repraance <reprance@gmail.com>
2023-07-06 19:55:06 +0800

    Revert "feat: external-internal-libs"
    This reverts commit 2bfd9a94d4eefd09b76b9e16cbd4c50b6eaa4b84.

--- packages/platform-web/src/node/index.ts
+++ packages/platform-web/src/node/index.ts
@@ -4,7 +4,6 @@ import {
   getPresetRuntimeFilesCreator
 } from '@shuvi/platform-shared/node';
 import tracePlugin from './trace';
-import externalInternalLibs from './external-internal-libs';
 import {
   getPlugins,
   getMiddlewares,
@@ -31,7 +30,6 @@ const platform =
         resolvePkgFile('shuvi-image.d.ts')
       ],
       plugins: [
-        externalInternalLibs,
         tracePlugin,
         ...SharedPlugins,
         ...getPlugins(platformContext),
