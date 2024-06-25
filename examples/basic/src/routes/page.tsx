68e0dc1f9b580a72c1e7d880529b1187ccb6cc16
Michael Hsu <evenchange4@gmail.com>
2024-06-25 16:39:41 +0800

    fix: fix ts5 no type errors issue

--- examples/basic/src/routes/page.tsx
+++ examples/basic/src/routes/page.tsx
@@ -1,9 +1,6 @@
 import styles from './style.css';
-function con(a: boolean) {
-  console.log(a);
-}
+
 const Home = () => {
-  con(1)
   return (
     <div className={styles.hello}>
       <p>Hello World</p>
