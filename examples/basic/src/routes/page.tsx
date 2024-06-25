1f1d5e2bfbe6ec7deed4bfa7ed65a568f1358f6a
Michael Hsu <evenchange4@gmail.com>
2024-06-25 16:20:23 +0800

    chore: reproduce issue ts5

--- examples/basic/src/routes/page.tsx
+++ examples/basic/src/routes/page.tsx
@@ -1,6 +1,9 @@
 import styles from './style.css';
-
+function con(a: boolean) {
+  console.log(a);
+}
 const Home = () => {
+  con(1)
   return (
     <div className={styles.hello}>
       <p>Hello World</p>
