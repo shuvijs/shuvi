583da64957814b8fcb15a275a579b9a7ecd3a235
Michael Hsu <evenchange4@gmail.com>
2024-06-28 13:23:27 +0800

    chore: refactor module system

--- examples/basic/src/routes/page.tsx
+++ examples/basic/src/routes/page.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import styles from './style.css';
 
 const Home = () => {
--- packages/service/src/project/file-presets/files/app/shuvi-app.d_ts.ts
+++ packages/service/src/project/file-presets/files/app/shuvi-app.d_ts.ts
@@ -4,6 +4,7 @@ import { ProjectContext } from '../../../projectContext';
 export default defineFile({
   content: (context: ProjectContext) => {
     const types = context.typeDeclarationFiles;
+    console.log('[Michael] types', types);
     return types
       .map(file => {
         const isPath = /\.d\.ts$/.test(file) || file.startsWith('.');
