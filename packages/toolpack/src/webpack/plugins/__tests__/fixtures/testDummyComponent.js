8fe82aed071704b8ec5423cc594a52c568e4f4af
Michael Hsu <evenchange4@gmail.com>
2025-06-25 13:42:36 +0800

    feat(module-replace-plugin): basic done

--- packages/toolpack/src/webpack/plugins/__tests__/fixtures/testDummyComponent.js
+++ packages/toolpack/src/webpack/plugins/__tests__/fixtures/testDummyComponent.js
@@ -1,3 +1,28 @@
+// Enhanced test dummy component for comprehensive testing
 export default function () {
   return 'Dummy';
 }
+
+export const namedExport = 'Named Export';
+
+export function testFunction() {
+  return 'Test Function';
+}
+
+export class TestClass {
+  constructor() {
+    this.name = 'TestClass';
+  }
+
+  getValue() {
+    return this.name;
+  }
+}
+
+export const testObject = {
+  name: 'Test Object',
+  value: 42,
+  method() {
+    return this.value;
+  }
+};
