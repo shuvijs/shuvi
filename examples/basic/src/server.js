b6064642fc2c1b887332283c7837ed216b7bcc2c
Michael Hsu <evenchange4@gmail.com>
2025-06-26 11:44:40 +0800

    chore: debug multiple init issue

--- examples/basic/src/server.js
+++ examples/basic/src/server.js
@@ -1,5 +1,22 @@
 export const handlePageRequest = originalHandlePageRequest => {
   return async (req, res) => {
+    // Pretty print the request information with colors
+    const colors = {
+      GET: '\x1b[32m', // Green
+      POST: '\x1b[34m', // Blue
+      PUT: '\x1b[33m', // Yellow
+      DELETE: '\x1b[31m', // Red
+      PATCH: '\x1b[35m', // Magenta
+      reset: '\x1b[0m', // Reset
+      underline: '\x1b[4m' // Underline
+    };
+
+    const methodColor = colors[req.method] || colors.reset;
+    const fullUrl = `${req.headers.host}${req.url}`;
+    console.debug(
+      `${methodColor}[${req.method}]${colors.reset} ${colors.underline}${fullUrl}${colors.reset}`
+    );
+
     /**
      * Delay page request by 500ms to simulate a slow server
      * This is useful for testing race conditions.
