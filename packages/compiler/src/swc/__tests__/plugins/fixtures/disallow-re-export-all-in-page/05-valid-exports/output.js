aacdd9c67ff5379b56a3051f076bab6f5b1c11a9
Michael Hsu <evenchange4@gmail.com>
2025-06-27 16:17:13 +0800

    test(disallow-re-report-all-in-page): add fixtures

---
+++ packages/compiler/src/swc/__tests__/plugins/fixtures/disallow-re-export-all-in-page/05-valid-exports/output.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx } from 'react/jsx-runtime';
+import React from 'react';
+export default function Page() {
+  return /*#__PURE__*/ _jsx('div', {
+    children: 'Hello'
+  });
+}
+export { Button } from './components';
