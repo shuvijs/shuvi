6783eefa1f5fd2eaecd3a056a04e03c526525397
Michael Hsu <evenchange4@gmail.com>
2025-06-30 20:36:05 +0800

    test(optimize-hook-destructuring): add fixtures

---
+++ packages/compiler/src/swc/__tests__/plugins/fixtures/optimize-hook-destructuring/07-default-value/input.ts
@@ -0,0 +1,2 @@
+import { useState } from 'react';
+const [count = 1, setCount] = useState(0); 
---
+++ packages/compiler/src/swc/__tests__/plugins/fixtures/optimize-hook-destructuring/07-default-value/output.js
@@ -0,0 +1,2 @@
+import { useState } from "react";
+var ref = useState(0), tmp = ref[0], count = tmp === void 0 ? 1 : tmp, setCount = ref[1];
